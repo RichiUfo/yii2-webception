cef1b573c613daf010ae48cad8a70f01b7c83421
Theophile Godard <theo.godard@gmail.com>
2015-05-30 15:20:22 +0900

    Replacing the app namespace by the frontend

--- Module.php
+++ Module.php
@@ -4,7 +4,7 @@ namespace frontend\modules\accounting;
 
 class Module extends \frontend\components\Module
 {
-    public $controllerNamespace = 'app\modules\accounting\controllers';
+    public $controllerNamespace = 'frontend\modules\accounting\controllers';
 	
     public function init()
     {
