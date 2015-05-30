851044c03c669437b9479f07626d28af9d385340
Theophile Godard <theo.godard@gmail.com>
2015-05-31 02:55:48 +0900

    Modifying the activerecord class

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -7,12 +7,6 @@ class ActiveRecord extends \frontend\components\ActiveRecord
 	
     public static function getDb()
 	{
-        /*$db = new \yii\db\Connection([
-			'dsn' => 'mysql:dbname=fullplanner2_accounting;host=localhost',
-			'username' => 'accounting',
-			'password' => 'lECwn3sqj3_dv-X37fpHxdntrR0m0fWx',
-		]);
-		$db->open();*/
 		return \Yii::$app->controller->module->db;
    	}
 
