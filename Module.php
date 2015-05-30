f79eb9e4be18950da6ba1d5eae53614609d6c091
Theophile Godard <theo.godard@gmail.com>
2015-05-30 20:02:27 +0900

    Adding test database

--- Module.php
+++ Module.php
@@ -5,11 +5,41 @@ namespace frontend\modules\accounting;
 class Module extends \frontend\components\Module
 {
     public $controllerNamespace = 'frontend\modules\accounting\controllers';
-	
-    public function init()
-    {
-        parent::init();
 
-        // custom initialization code goes here
+	/**
+     * Constructor.
+     * @param string $id the ID of this module
+     * @param Module $parent the parent module (if any)
+     * @param array $config name-value pairs that will be used to initialize the object properties
+     *
+    public function __construct($id, $parent = null, $config = [])
+    {
+        parent::__construct($id, $parent, $config);
+		
+		// Open the database connection for the module
+		$this->db = new \yii\db\Connection([
+			'dsn' => 'mysql:dbname='.$this->db_name.';host=localhost',
+			'username' => $this->db_username,
+			'password' => $this->db_password,
+		]);
+		$this->db->open();
+		
     }
+	
+	/**
+     * Constructor.
+     * @param string $id the ID of this module
+     * @param Module $parent the parent module (if any)
+     * @param array $config name-value pairs that will be used to initialize the object properties
+     */
+    public function __construct($id, $parent = null, $config = [])
+    {
+        $this->db_name = 'fullplanner2_accounting'
+        $this->db_username = 'accounting'
+        $this->db_password = 'lECwn3sqj3_dv-X37fpHxdntrR0m0fWx'
+			
+		parent::__construct($id, $parent, $config);
+		
+    }
+		
 }
