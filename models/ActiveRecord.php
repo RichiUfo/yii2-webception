77da206fddca7294db882896bac4f58ba439faaa
Theophile Godard <theo.godard@gmail.com>
2015-05-30 15:11:47 +0900

    Updating the DB credentials

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -8,7 +8,7 @@ class ActiveRecord extends \frontend\components\ActiveRecord
     public static function getDb()
 	{
         $db = new \yii\db\Connection([
-			'dsn' => 'mysql:dbname=fullplanner2_template;host=127.0.0.1',
+			'dsn' => 'mysql:dbname=fullplanner2_accounting;host=localhost',
 			'username' => 'accounting',
 			'password' => 'lECwn3sqj3_dv-X37fpHxdntrR0m0fWx',
 		]);
