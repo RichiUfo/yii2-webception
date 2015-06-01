29f17d749633dc09f35ad4e5dd570273d7abdaf7
Theophile Godard <theo.godard@gmail.com>
2015-06-01 21:54:36 +0900

    Autocommit

--- assets/javascript/create-transaction.js
+++ assets/javascript/create-transaction.js
@@ -8,7 +8,7 @@ app.controller("FormCtrl", function($scope, $http) {
 	
 	// Get the account information when changed
 	$scope.$watch(
-		function(scope) { return [scope.account_debit_id, scope.account_credit_id]; }
+		account_debit_id
 		,function(media) {
 			console.log('Account Change Detected !');
 		}
