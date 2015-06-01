c3ec74cfc7470cc0b840a3dbe3201b7ffcd6b895
Theo Godard <theo.godard@gmail.com>
2015-06-01 03:01:15 +0000

    Autocommit

--- controllers/AccountForexController.php
+++ controllers/AccountForexController.php
@@ -37,7 +37,7 @@ class AccountForexController extends \frontend\components\Controller
 	public function actionDisplayForexAccount() {
 		$forex = AccountForex::find()
 			//->with('account')
-			->where(['account' => ])
+			->where(['accounts.owner_id' => 2])
 			->all();
 		return $this->render('display', [
 			'forex' => $forex,
