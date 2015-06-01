908089ddf97c346cec6eea1f0349f2330ffff3ac
Theo Godard <theo.godard@gmail.com>
2015-06-01 03:19:03 +0000

    Autocommit

--- views/account-forex/display-all.php
+++ views/account-forex/display-all.php
@@ -22,14 +22,14 @@ BaseAsset::register($this);
         <table class="table table-striped">
 			<thead>
 				<tr>
-					<th>Account</th>
+					<th>Account (owner_id)</th>
 					<th class="text-right">Value</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php foreach($forex as $account) : ?>
 				<tr>
-					<td><?= $account->account->alias ?></td>
+					<td><?= $account->account->alias ?> (<?= $account->account->owner_id ?>)</td>
 					<td class="text-right"><?= $account->account->display_value ?></td>
 				</tr>
 				<?php endforeach; ?>
