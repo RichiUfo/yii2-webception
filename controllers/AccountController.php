a18f57b334d20429033f800142f73866c8f782f2
Theo Godard <theo.godard@gmail.com>
2015-06-02 14:01:55 +0000

    Autocommit

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -298,7 +298,7 @@ class AccountController extends \frontend\components\Controller
     public function actionUpdate() {
         
         \Yii::$app->response->format = 'json';
-        return Yii::$app->request->post();
+        return var_dump(Yii::$app->request->post());
         
         $id = (int)Yii::$app->request->post('id');
         $property = Yii::$app->request->post('property');
