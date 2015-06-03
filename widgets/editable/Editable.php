79960f74f69ebeea2374fa40278614567c74f49a
Theo Godard <theo.godard@gmail.com>
2015-06-03 13:29:17 +0000

    Autocommit

--- widgets/editable/Editable.php
+++ widgets/editable/Editable.php
@@ -54,26 +54,27 @@ class Editable extends \yii\base\Widget{
 		    'id' => 'account-title-edit-form', 
 		    'class' => 'form-inline'
 	    ]); 
-        echo Html::input('hidden', 'id', $this->identifier);
-        echo Html::input('text', 'value', '', [
-            'id' => 'account-title-edit-form-field',
-            'class' => 'form-field-invisible text-center h1',
-            'ng-show' => 'editionMode',
-            'ng-model' => 'value'
-        ]);
-        echo Html::beginTag('div', ['class' => 'buttons-line pull-right form-inline']);
-        echo Html::button('Default', [
-            'id' => 'account-title-edit-form-reset-button',
-            'class' => 'btn btn-xs',
-            'ng-click' => 'reset()'
-        ]); 
-        echo Html::button('Save', [
-            'id' => 'account-title-edit-form-button',
-            'class' => 'btn btn-xs btn-primary',
-            'ng-click' => 'save()'
-        ]);
-        echo Html::endTag('div');
-        
+    	    echo Html::beginTag('div', ['class' => 'form-container']);
+                echo Html::input('hidden', 'id', $this->identifier);
+                echo Html::input('text', 'value', '', [
+                    'id' => 'account-title-edit-form-field',
+                    'class' => 'form-field-invisible text-center h1',
+                    'ng-show' => 'editionMode',
+                    'ng-model' => 'value'
+                ]);
+                echo Html::beginTag('div', ['class' => 'buttons-line pull-right form-inline']);
+                    echo Html::button('Default', [
+                        'id' => 'account-title-edit-form-reset-button',
+                        'class' => 'btn btn-xs',
+                        'ng-click' => 'reset()'
+                    ]); 
+                    echo Html::button('Save', [
+                        'id' => 'account-title-edit-form-button',
+                        'class' => 'btn btn-xs btn-primary',
+                        'ng-click' => 'save()'
+                    ]);
+                echo Html::endTag('div');
+            echo Html::endTag('div');
         $form->end();
         
         echo Html::endTag('div');
