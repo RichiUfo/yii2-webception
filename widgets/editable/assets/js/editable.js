1b7e23aca2d4ca84d45f9d3383b7a740760bfa9c
Theo Godard <theo.godard@gmail.com>
2015-06-03 15:07:49 +0000

    Autocommit

--- widgets/editable/assets/js/editable.js
+++ widgets/editable/assets/js/editable.js
@@ -4,29 +4,38 @@ app.controller("EditableController", function($scope, $http, $window) {
 	
 	// Variables
 	$scope.editionMode = false;
+	$scope.resetting = false;
 	$scope.id = $window.fpEditableId;
 	$scope.action = $window.fpEditableAction;
 	$scope.value = $window.fpEditableInitial;
 	$scope.default_value = $window.fpEditableDefault;
 	
 	$scope.reset = function(){
+	    $scope.resetting = true;
 	    $scope.value = $scope.default_value;
 	}
 	
 	$scope.save = function(){
-	    $scope.editionMode = false;
+	    $scope.resetting = false;
+	    $timeout(function () {          // will be executed after ngClick function in case of click
+            if ($scope.clicked) {
+                return;
+            }
 	    
-	    // Build the update data
-	    $scope.data = {};
-	    $scope.data['id'] = $scope.id;
-	    $scope.data['alias'] = $scope.value;
-	    console.log($scope.data);
-	    
-	    // Send the update request
-	    $http.post($scope.action, $scope.data)
-	    .success(function(data, status, headers, config) {
-            console.log('Create a Notification here', data);
-        });
+    	    $scope.editionMode = false;
+    	    
+    	    // Build the update data
+    	    $scope.data = {};
+    	    $scope.data['id'] = $scope.id;
+    	    $scope.data['alias'] = $scope.value;
+    	    console.log($scope.data);
+    	    
+    	    // Send the update request
+    	    $http.post($scope.action, $scope.data)
+    	    .success(function(data, status, headers, config) {
+                console.log('Create a Notification here', data);
+            });
+	    });
 	}
 	
 });
