277673b4ba722fee63f6a37ab0662806927972dd
Theo Godard <theo.godard@gmail.com>
2015-06-03 23:26:34 +0000

    Autocommit

--- widgets/editable/assets/js/editable.js
+++ widgets/editable/assets/js/editable.js
@@ -14,22 +14,21 @@ app.controller("EditableController", function($scope, $http, $window, $timeout) 
 	}
 	
 	$scope.save = function(){
-	    //$scope.resetting = false;
-	    $timeout(function () {
-    	    $scope.editionMode = false;
-    	    
-    	    // Build the update data
-    	    $scope.data = {};
-    	    $scope.data['id'] = $scope.id;
-    	    $scope.data['alias'] = $scope.value;
-    	    console.log($scope.data);
-    	    
-    	    // Send the update request
-    	    $http.post($scope.action, $scope.data)
-    	    .success(function(data, status, headers, config) {
-                console.log('Create a Notification here', data);
-            });
-	    }, 100);
+	    $scope.editionMode = false;
+	    
+	    if($scope.value != $window.fpEditableInitial){
+		    // Build the update data
+		    $scope.data = {};
+		    $scope.data['id'] = $scope.id;
+		    $scope.data['alias'] = $scope.value;
+		    console.log($scope.data);
+		    
+		    // Send the update request
+		    $http.post($scope.action, $scope.data)
+		    .success(function(data, status, headers, config) {
+	            console.log('Create a Notification here', data);
+	        });
+	    }
 	}
 	
 });
