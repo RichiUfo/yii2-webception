1aab9b8d17f92112fe0e8234814c730e190bd43f
Theo Godard <theo.godard@gmail.com>
2015-06-03 02:41:20 +0000

    Autocommit

---
+++ controllers/AccountrestController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace frontend\modules\accounting\controllers;
+
+use yii\rest\ActiveController;
+
+class AccountrestController extends ActiveController
+{
+    public $modelClass = 'frontend\modules\accounting\models\Account';
+}
