e0a963612bb6d6d3b354ce97b58190f9774be1ac
Theo Godard <theo.godard@gmail.com>
2015-06-03 12:21:26 +0000

    Autocommit

--- widgets/editable/assets/js/editable.js
+++ widgets/editable/assets/js/editable.js
@@ -1,36 +1,3 @@
-// Use Case : User click on the editable title
-/*$('#account-title').on('click', function(event) {
-    $('#account-title').hide();
-    $('#account-title-edit').show();
-});
-
-// Use Case : User click on the save button
-$('#account-title-edit-form-button').on('click', function(event) {
-    if($('#account-title-edit-form-field').val() === ''){
-        $('#account-title-value').html('".$account->name."');
-    }
-    else {
-        $('#account-title-value').html($('#account-title-edit-form-field').val());
-    }
-    $('#account-title').show();
-    $('#account-title-edit').hide();
-    
-    $.ajax({
-        method: 'POST',
-        url: '/accounting/account/rename',
-        data: $('#account-title-edit-form').serializeArray(),
-        success: function(result){
-            ToastrAjaxFeed.getNotifications('/notification/get-notifications', {'positionClass':'toast-bottom-right'});
-        }
-    });
-});
-
-// Use Case : User click on the default button
-$('#account-title-edit-form-reset-button').on('click', function(event) {
-    $('#account-title-edit-form-field').val('".$account->name."');
-});
-*/
-  
 var app = angular.module("editableApp", []);
 
 app.controller("EditableController", function($scope, $http, $window) {
@@ -40,7 +7,7 @@ app.controller("EditableController", function($scope, $http, $window) {
 	$scope.value = $window.fpEditableInitial;
 	
 	$scope.save = function(){
-	    $http.post('/accounting/account/update', {
+	    $http.post('/accounting/rest/account/auth', {
 	        id: '100',
 	        property: 'alias',
 	        value: $scope.value,
