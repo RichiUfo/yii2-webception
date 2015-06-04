61564130081e8f76ad55ebf053e2f8bff8c89353
Theo Godard <theo.godard@gmail.com>
2015-06-04 06:49:57 +0000

    Autocommit

--- views/transaction/create.php
+++ views/transaction/create.php
@@ -11,135 +11,134 @@ BaseAsset::register($this);
 
 ?>
 
-<div id="trans-create-app" class="fp-angular">
-	<div ng-controller="FormController" class="transaction-form">
-		<?php $form = ActiveForm::begin([
-			'id' => 'create-transaction-form',
-			'enableClientValidation' => false
-		]); ?>
+<div id="transaction-create-app" ng-controller="FormController" class="transaction-form">
+	
+	<?php $form = ActiveForm::begin([
+		'id' => 'create-transaction-form',
+		'enableClientValidation' => false
+	]); ?>
 
-		<?php StepFormWidget::begin([
-			'id' => 'formtest',
-			'header' => [
-				'title_strong' => 'NEW',
-				'title' => 'ACCOUNTING TRANSACTION',
-				'subtitle' => 'Use this form to report a general accounting transaction'
-				],
-			'steps' => [
-				'accounts' => 'Accounts',
-				'details' => 'Details',
-				'summary' => 'Summary'
-				]
-			]); ?>  
+	<?php StepFormWidget::begin([
+		'id' => 'formtest',
+		'header' => [
+			'title_strong' => 'NEW',
+			'title' => 'ACCOUNTING TRANSACTION',
+			'subtitle' => 'Use this form to report a general accounting transaction'
+			],
+		'steps' => [
+			'accounts' => 'Accounts',
+			'details' => 'Details',
+			'summary' => 'Summary'
+			]
+		]
+	); ?>  
 
-			<div class="tab-pane" id="accounts">
-				<div class="row">
-					<h4 class="info-text">Pick the accounts</h4>
-					<div class="col-sm-5 col-sm-offset-1 acccount-select-group">
-						<?= $form
-							->field($model, 'account_debit_id')
-							->dropDownList($accounts, [
-								'class' => 'form-control account-select', 
-								'ng-model' => 'account_debit_id',
-								'prompt' => 'Select An Account'
-							]) ?>
-						<div class="account-summary">
-							<div class="name">{{account_debit.name}} 
-								<span class="pull-right">{{account_debit.display_value}} {{account_debit.currency.code}}</span>
-							</div>
-							<div class="root"><img src="<?= Url::to('@web/img/flags/48/') ?>{{account_debit.currency.img}}"></div>
+		<div class="tab-pane" id="accounts">
+			<div class="row">
+				<h4 class="info-text">Pick the accounts</h4>
+				<div class="col-sm-5 col-sm-offset-1 acccount-select-group">
+					<?= $form
+						->field($model, 'account_debit_id')
+						->dropDownList($accounts, [
+							'class' => 'form-control account-select', 
+							'ng-model' => 'account_debit_id',
+							'prompt' => 'Select An Account'
+						]) ?>
+					<div class="account-summary">
+						<div class="name">{{account_debit.name}} 
+							<span class="pull-right">{{account_debit.display_value}} {{account_debit.currency.code}}</span>
 						</div>
-					</div>
-					<div class="col-sm-5 acccount-select-group">
-						<?= $form
-							->field($model, 'account_credit_id')
-							->dropDownList($accounts, [
-								'class' => 'form-control account-select', 
-								'ng-model' => 'account_credit_id',
-								'prompt' => 'Select An Account'
-							]) ?>
-						<div class="account-summary">
-							<div class="name">{{account_credit.name}} 
-								<span class="pull-right">{{account_credit.display_value}} {{account_credit.currency.code}}</span>
-							</div>
-							<div class="root"><img src="<?= Url::to('@web/img/flags/48/') ?>{{account_credit.currency.img}}"></div>
-						</div>
+						<div class="root"><img src="<?= Url::to('@web/img/flags/48/') ?>{{account_debit.currency.img}}"></div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-sm-10 col-sm-offset-1">
-						<p class="feedback info-text text-center"></p>
+				<div class="col-sm-5 acccount-select-group">
+					<?= $form
+						->field($model, 'account_credit_id')
+						->dropDownList($accounts, [
+							'class' => 'form-control account-select', 
+							'ng-model' => 'account_credit_id',
+							'prompt' => 'Select An Account'
+						]) ?>
+					<div class="account-summary">
+						<div class="name">{{account_credit.name}} 
+							<span class="pull-right">{{account_credit.display_value}} {{account_credit.currency.code}}</span>
+						</div>
+						<div class="root"><img src="<?= Url::to('@web/img/flags/48/') ?>{{account_credit.currency.img}}"></div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<p class="feedback info-text text-center"></p>
+				</div>
+			</div>
+		</div>
+
+		<div class="tab-pane" id="details">
+			<div class="row">
+				<div class="col-sm-5 col-sm-offset-1">
+					<?= $form->field($model, 'date_value')
+						->widget(DatePicker::classname(), [
+							'options' => [
+								'placeholder' => '',
+								'style' => 'display:none'
+							],  
+							'type' => DatePicker::TYPE_INLINE,
+							'removeButton' => false,
+							'size' => 'sm',
+							'pluginOptions' => [
+								'autoclose'=>true,
+								'format' => 'yyyy/mm/dd'
+							]
+						]);
+					?>
+				</div>
+				<div class="col-sm-5">
+					<?= $form->field($model, 'name', ['options' => [
+						'style'=>'margin-bottom:30px'
+					]]) ?>
+
+					<?= $form->field($model, 'description', ['options' => [
+						'style'=>'margin-bottom:30px'
+					]]) ?>
+
+					<div ng-if="account_credit.currency.code == account_debit.currency.code">
+						<?= $form->field($model, 'value') ?>
+					</div>
+					
+					<div ng-if="account_credit.currency.code != account_debit.currency.code">
+						<?= Html::input("text", "value_debit"); ?>
+						<?= Html::input("text", "value_credit"); ?>
 					</div>
 				</div>
 			</div>
 
-			<div class="tab-pane" id="details">
-				<div class="row">
-					<div class="col-sm-5 col-sm-offset-1">
-						<?= $form->field($model, 'date_value')
-							->widget(DatePicker::classname(), [
-								'options' => [
-									'placeholder' => '',
-									'style' => 'display:none'
-								],  
-								'type' => DatePicker::TYPE_INLINE,
-								'removeButton' => false,
-								'size' => 'sm',
-								'pluginOptions' => [
-									'autoclose'=>true,
-									'format' => 'yyyy/mm/dd'
-								]
-							]);
-						?>
-					</div>
-					<div class="col-sm-5">
-						<?= $form->field($model, 'name', ['options' => [
-							'style'=>'margin-bottom:30px'
-						]]) ?>
-
-						<?= $form->field($model, 'description', ['options' => [
-							'style'=>'margin-bottom:30px'
-						]]) ?>
-
-						<div ng-if="account_credit.currency.code == account_debit.currency.code">
-							<?= $form->field($model, 'value') ?>
-						</div>
-						
-						<div ng-if="account_credit.currency.code != account_debit.currency.code">
-							<?= Html::input("text", "value_debit"); ?>
-							<?= Html::input("text", "value_credit"); ?>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="col-sm-10 col-sm-offset-1">
-						<p class="feedback info-text text-center"></p>
-					</div>
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<p class="feedback info-text text-center"></p>
 				</div>
 			</div>
+		</div>
 
-			<div class="tab-pane" id="summary">
-				<div class="row">
-					<div class="col-sm-12">
-						<h4 class="info-text">Confirm the transaction ?</h4>
-					</div>
-					<div class="col-sm-7 col-sm-offset-1">
-						 <div class="form-group">
-							<label>debitaccount</label>
-						  </div>
-					</div>
-					<div class="col-sm-3">
-						 <div class="form-group">
-							<label>Some info</label>
-						  </div>
-					</div>
+		<div class="tab-pane" id="summary">
+			<div class="row">
+				<div class="col-sm-12">
+					<h4 class="info-text">Confirm the transaction ?</h4>
+				</div>
+				<div class="col-sm-7 col-sm-offset-1">
+					 <div class="form-group">
+						<label>debitaccount</label>
+					  </div>
+				</div>
+				<div class="col-sm-3">
+					 <div class="form-group">
+						<label>Some info</label>
+					  </div>
 				</div>
 			</div>
-			<?php StepFormWidget::end(); ?>
-		<?php ActiveForm::end(); ?>
+		</div>
+		<?php StepFormWidget::end(); ?>
+	<?php ActiveForm::end(); ?>
 
-		<!--$this->registerJs('transaction_create_fom_init()'); -->
-		<?php $this->registerJs('angular_activator("trans-create-app", "transactionCreateApp")'); ?>
-	</div>
+	<?php $this->registerJs('fp_angular_bootstrap("transaction-create-app", ["transactionCreateApp"])'); ?>
 </div>
