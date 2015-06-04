b1e3e055c817d26fde9c96bfd68aeff8be4b78f8
Theo Godard <theo.godard@gmail.com>
2015-06-04 06:52:34 +0000

    Autocommit

--- assets/BaseAsset.php
+++ assets/BaseAsset.php
@@ -20,8 +20,8 @@ class BaseAsset extends AssetBundle
     ];
     public $js = [
         'javascript/module_accounting.js',
-		//'javascript/accounting_transaction_create.js',
-		'javascript/create-transaction.js',
+		'javascript/accounting_transaction_create.js',
+		//'javascript/create-transaction.js',
     ];
     public $depends = [
 		'frontend\assets\angular\AngularAsset',
