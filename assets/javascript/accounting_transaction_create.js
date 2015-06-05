e7b74adf5bab83c0db3cfe507a89cfd7a176638a
Theo Godard <theo.godard@gmail.com>
2015-06-05 01:32:22 +0000

    Autocommit

--- assets/javascript/accounting_transaction_create.js
+++ assets/javascript/accounting_transaction_create.js
@@ -15,13 +15,19 @@ app.controller("FormController", function($scope, $http) {
 		$http.get('/accounting/account/get-account-summary', {
 			params: { accountid: $scope.account_debit_id }
 		})
-		.success(function(data, status, headers, config) { $scope.account_debit = data; });
+		.success(function(data, status, headers, config) { 
+			$scope.account_debit = data; 
+			$scope.checkAccounts();
+		});
 	}); 
 	$scope.$watch('account_credit_id', function(value) {
 		$http.get('/accounting/account/get-account-summary', {
 			params: { accountid: $scope.account_credit_id }
 		})
-		.success(function(data, status, headers, config) { $scope.account_credit = data; });
+		.success(function(data, status, headers, config) { 
+			$scope.account_credit = data; 
+			$scope.checkAccounts();
+		});
 	});
 	
 	$scope.checkAccounts = function() {
