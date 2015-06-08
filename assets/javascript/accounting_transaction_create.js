c0ea5d293a5994fc492a9788625739c67dbd9624
Theo Godard <theo.godard@gmail.com>
2015-06-08 05:26:51 +0000

    Autocommit

--- assets/javascript/accounting_transaction_create.js
+++ assets/javascript/accounting_transaction_create.js
@@ -3,7 +3,7 @@
  */
 var app = angular.module("transactionCreateApp", []);
 
-app.controller("FormController", [$filters, function($scope, $http, $filters) {
+app.controller("FormController", [$scope, $http, $filters, function($scope, $http, $filters) {
 	
 	// Variables
 	$scope.account_debit = null;
