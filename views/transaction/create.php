64101bfbf31baeb19e23387c324d61e93abb945e
Theo Godard <theo.godard@gmail.com>
2015-06-08 07:58:56 +0000

    Autocommit

--- views/transaction/create.php
+++ views/transaction/create.php
@@ -156,7 +156,7 @@ BaseAsset::register($this);
 
 		<div class="row">
 			<div class="col-sm-10 col-sm-offset-1">
-				<p class="feedback info-text text-center">{{credit_value | number:2}}</p>
+				<p class="feedback info-text text-center"></p>
 			</div>
 		</div>
 	</div>
