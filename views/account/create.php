9d1d3fc261a15f222934bec6d377925d51f1a0ef
Theo Godard <theo.godard@gmail.com>
2015-06-09 07:09:46 +0000

    Autocommit

--- views/account/create.php
+++ views/account/create.php
@@ -9,60 +9,63 @@ use frontend\widgets\stepform\StepForm;
 
 ?>
 
-<?php
-$stepform = StepForm::begin([
-	'id' => 'create-account',
-	'activeFormOptions' => [
-		'id' => 'create-account-form',
-		'enableClientValidation' => false
-	],
-	'header' => [
-		'title_strong' => 'NEW',
-		'title' => 'ACCONUT',
-		'subtitle' => 'General Account Creation'
-		],
-	'steps' => [
-		'step1' => 'Parent',
-		'step2' => 'Account',
-		'step3' => 'Summary'
-		]
-	]
-);
-?>
-
-    <div class="tab-pane" id="step1">
-        <div class="row">
-            <h4 class="info-text">Choose a parent</h4>
-          
-            <div class="col-sm-10 col-sm-offset-1">
-                <?= $stepform->form
-                ->field($model, 'parent_id')
-                ->dropDownList($accounts, [
-                    'class' => 'form-control account-select', 
-                    'prompt' => 'Select An Account'
-                ]) ?>
+<div id="account-create-app" ng-controller="FormController" class="transaction-form">
+    <?php
+    $stepform = StepForm::begin([
+    	'id' => 'create-account',
+    	'activeFormOptions' => [
+    		'id' => 'create-account-form',
+    		'enableClientValidation' => false
+    	],
+    	'header' => [
+    		'title_strong' => 'NEW',
+    		'title' => 'ACCONUT',
+    		'subtitle' => 'General Account Creation'
+    		],
+    	'steps' => [
+    		'step1' => 'Parent',
+    		'step2' => 'Account',
+    		'step3' => 'Summary'
+    		]
+    	]
+    );
+    ?>
+    
+        <div class="tab-pane" id="step1">
+            <div class="row">
+                <h4 class="info-text">Choose a parent</h4>
+              
+                <div class="col-sm-10 col-sm-offset-1">
+                    <?= $stepform->form
+                    ->field($model, 'parent_id')
+                    ->dropDownList($accounts, [
+                        'class' => 'form-control account-select', 
+                        'prompt' => 'Select An Account'
+                    ]) ?>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="tab-pane" id="step2">
-        <h4 class="info-text">Give it a name</h4>
-        <div class="row">
-            <div class="col-sm-10 col-sm-offset-1">
-                <?= $stepform->form->field($model, 'name') ?>
-                <?= $stepform->form->field($model, 'currency') ?>
+    
+        <div class="tab-pane" id="step2">
+            <h4 class="info-text">Give it a name</h4>
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <?= $stepform->form->field($model, 'name') ?>
+                    <?= $stepform->form->field($model, 'currency') ?>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="tab-pane" id="step3">
-        <div class="row">
-            <div class="col-sm-12">
-                <h4 class="info-text">Confirm That !</h4>
-            </div>
-            <div class="col-sm-10 col-sm-offset-1">
-                 
+    
+        <div class="tab-pane" id="step3">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="info-text">Confirm That !</h4>
+                </div>
+                <div class="col-sm-10 col-sm-offset-1">
+                     
+                </div>
             </div>
         </div>
-    </div>
-<?php $stepform->end() ?>
+        
+    <?php $stepform->end() ?>
+</div>
