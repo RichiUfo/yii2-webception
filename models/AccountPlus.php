4e03d6e8fadc189061ac27dd9e71083823e63c54
Theo Godard <theo.godard@gmail.com>
2015-06-09 05:50:14 +0000

    Autocommit

--- models/AccountPlus.php
+++ models/AccountPlus.php
@@ -19,7 +19,8 @@ class AccountPlus extends Account
         
         parent::afterFind();
 
-        $this->root_account = $this->parent_id;
+        $rootid = $this->parent_id;
+        $this->root_account = $this;
         //while($this->root_account->parent_id != 0) {
         //    $this->root_account = AccountPlus::findOne($this->root_account->parent_id);
         //}
