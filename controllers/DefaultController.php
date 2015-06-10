a52534a1b4d9cccff19536fc59127471fdbb8b78
Theo Godard <theo.godard@gmail.com>
2015-06-10 05:23:51 +0000

    Autocommit

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -61,7 +61,7 @@ class DefaultController extends \frontend\components\Controller
 	public function actionTest() { 
 		
 		$rate = ExchangeController::get('finance','forex-rate', [
-		    'date' => '2015-05-19',    
+		    'date' => '2015-05-25',    
 		    'from' => 'EUR',    
 		    'to' => 'KRW',    
 	    ]);
