29eefb1671ce3c2b1e6ad1a27c1eb38361d93446
Theo Godard <theo.godard@gmail.com>
2015-06-10 09:38:43 +0000

    Autocommit

--- controllers/TransactionController.php
+++ controllers/TransactionController.php
@@ -201,10 +201,10 @@ class TransactionController extends \frontend\components\Controller
             
             // STEP 2 - Create the regular transaction
             if($credit_currency === $system_currency) {
-                $transaction = createTransactionRegular($trading->account, $credit, $value, $date, $name, $description);
+                $transaction = TransactionController::createTransactionRegular($trading->account, $credit, $value, $date, $name, $description);
             }
             else if($debit_currency === $system_currency) {
-                $transaction = createTransactionRegular($debit, $trading->account, $value, $date, $name, $description);
+                $transaction = TransactionController::createTransactionRegular($debit, $trading->account, $value, $date, $name, $description);
             }
             
             // STEP 3 - Create the forex account
