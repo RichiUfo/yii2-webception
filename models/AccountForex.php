d2b9ecd9139c2984e49d25b8a2d9286644f2a6de
Theo Godard <theo.godard@gmail.com>
2015-06-11 14:57:04 +0000

    Autocommit

--- models/AccountForex.php
+++ models/AccountForex.php
@@ -61,7 +61,7 @@ class AccountForex extends \frontend\components\ActiveRecord
             'to' => $currency_main,
         ]);
         
-        $this->value = $value_main - $value_forex_converted;
+        $this->value = $value_forex_converted - $value_main;
     }
     
     /**
