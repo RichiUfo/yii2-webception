d988c9c5e8ea2916ff53986c3dd0b45d78f42fdc
Theo Godard <theo.godard@gmail.com>
2015-06-13 11:01:35 +0000

    Autocommit

--- views/account-forex/test.php
+++ views/account-forex/test.php
@@ -1,4 +1,4 @@
-Amount of currency sold : <?= $res[3] ?> XYZ <br>
+Amount of currency sold : <?= $res[3] ?> XYZ <br><br>
 
 Realized = (Revenue from currency sales) - (Cost of currency sold)<br>
 <?= $res[0] ?> = <?= $res[1] ?> - <?= $res[2] ?>
