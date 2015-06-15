51cb58405497ae54cefc8cb125fc8b9674beef84
Theo Godard <theo.godard@gmail.com>
2015-06-15 12:09:00 +0000

    Autocommit

--- views/init/test.php
+++ views/init/test.php
@@ -7,7 +7,7 @@ function rec_disp($acc) {
    
     echo '<ul>';
     foreach($acc as $a){
-        echo '<li>'.$a['name'].' => '.isset($a['children']).'</li>';
+        echo '<li>'.$a['name'].' => '.empty($a['children']).'</li>';
         
         // Recursive
         if (isset($a['children']))
