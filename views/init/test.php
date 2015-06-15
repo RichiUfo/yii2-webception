fec2041b873ab08d50afff5add66ff27d80af9d8
Theo Godard <theo.godard@gmail.com>
2015-06-15 05:27:26 +0000

    Autocommit

---
+++ views/init/test.php
@@ -0,0 +1,24 @@
+<?php 
+
+$rec_disp = function ($acc) {
+    
+    // Display <li>
+    echo '<li>'.$acc->name.'</li>';
+    
+    // Recursive
+    if ($acc->accounts) {
+        echo '<ul>';
+        foreach($acc->accounts as $a)
+            rec_disp($a);
+        echo '</ul>';
+    }
+    
+}
+?>
+
+
+<h1>Chart of accounts</h1>
+
+<ul>
+<?php rec_disp($account); ?>
+</ul>
