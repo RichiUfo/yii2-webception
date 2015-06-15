8720fd8f10fa606a7ea864dfc71b526e59fc6e94
Theo Godard <theo.godard@gmail.com>
2015-06-15 12:12:19 +0000

    Autocommit

--- views/init/test.php
+++ views/init/test.php
@@ -10,6 +10,8 @@ function rec_disp($acc) {
         $test = isset($a['children'])?'True':'False';
         echo '<li>'.$a['name'].' => '.$test.'</li>'; 
         
+        var_dump($a); echo '<br><br>'; var_dump($a['children']);
+        
         // Recursive
         if (isset($a['children']))
             rec_disp($a['children']);
