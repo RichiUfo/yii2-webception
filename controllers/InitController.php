90859f9e72bdeab7f2b4a0ade3074416501e62dd
Theo Godard <theo.godard@gmail.com>
2015-06-15 14:51:44 +0000

    Autocommit

--- controllers/InitController.php
+++ controllers/InitController.php
@@ -37,46 +37,10 @@ class InitController extends \frontend\components\Controller
      * It create the necessary database entries for the basic features
      * */
     public function actionIndex(){      // To be removed, just for external call for debug
-        $this->initializeModule();
+        $this->initializeModule('personal');
     } 
-     
-    public function initializeModule2() {
-        
-        // Chart Of Acccounts - Personal Finance
-        $assets = AccountController::createAccount('Assets', 0, 1);
-            $fixed_assets = AccountController::createAccount('Fixed Assets', $assets->id, 1); 
-            $current_assets = AccountController::createAccount('Current Assets', $assets->id, 2);
-                $trading_accounts = AccountController::createAccount('Trading Accounts', $current_assets->id, 1);  
-                $savings_accounts = AccountController::createAccount('Savings Accounts', $current_assets->id, 2); 
-                $checking_accounts = AccountController::createAccount('Checking Accounts', $current_assets->id, 3); 
-                $cash = AccountController::createAccount('Cash', $current_assets->id, 4); 
-        
-        $equity = AccountController::createAccount('Equity', 0, 1);
-            $capital = AccountController::createAccount('Capital', $equity->id, 1);
-            $capital = AccountController::createAccount('Retained Earnings', $equity->id, 2);
-            $capital = AccountController::createAccount('Forex Unrealized Profits and Losses', $equity->id, 3);
-            
-        
-        $liabilities = AccountController::createAccount('Liabilities', 0, 1);
-            $lt_liabilities = AccountController::createAccount('Long-Term Liabilities', $liabilities->id, 1);
-                $mortgages = AccountController::createAccount('Mortgages', $lt_liabilities->id, 1);
-                $loans = AccountController::createAccount('Other Loans', $lt_liabilities->id, 2);
-            $current_liabilities = AccountController::createAccount('Current Liabilities', $liabilities->id, 2);
-                $accounts_payable = AccountController::createAccount('Accounts Payable', $current_liabilities->id, 1);
-                $credit_cards = AccountController::createAccount('Credit Cards', $current_liabilities->id, 2);
-        
-        $revenues = AccountController::createAccount('Revenues', 0, 1);
-            $operating_revenues = AccountController::createAccount('Operating Revenues', $revenues->id, 1);
-            $non_operating_revenues = AccountController::createAccount('Non-Operating Revenues And Gains', $revenues->id, 2);
-            
-        $expenses = AccountController::createAccount('Expenses', 0, 1);
-            $operating_expenses = AccountController::createAccount('Operating Expenses', $expenses->id, 1);
-            $non_operating_expenses = AccountController::createAccount('Non-Operating Expenses And Losses', $expenses->id, 2);
-        
-        return $this->render('index');
-    }
     
-    public function actionTest($chart) {
+    public function initializeModule($chart) {
         
         function create_rec($accounts, $parent_id = 0) {
             foreach($accounts as $account){
@@ -102,7 +66,7 @@ class InitController extends \frontend\components\Controller
         // Create or update the accounts
         create_rec($accounts);
         
-        return $this->render('test', [
+        return $this->render('index', [
             'chart' => $chart,
             'accounts' => $accounts
         ]);
