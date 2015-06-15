6c7540a23c3fedf586d5f465380c213655996b3f
Theo Godard <theo.godard@gmail.com>
2015-06-15 05:17:52 +0000

    Autocommit

--- views/account-forex/test.php
+++ views/account-forex/test.php
@@ -26,3 +26,23 @@ Account ID : <?= $res[0]->account->id ?><br><br>
     </tbody>
 </table>
 
+<?php
+libxml_use_internal_errors(true);
+$myXMLData =
+"<?xml version='1.0' encoding='UTF-8'?> 
+<document> 
+<user>John Doe</wronguser> 
+<email>john@example.com</wrongemail> 
+</document>"; 
+
+$xml = simplexml_load_string($myXMLData);
+if ($xml === false) {
+    echo "Failed loading XML: ";
+    foreach(libxml_get_errors() as $error) {
+        echo "<br>", $error->message;
+    }
+} else {
+    var_dump($xml);
+    print_r($xml);
+}
+?>
