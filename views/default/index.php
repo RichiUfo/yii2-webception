9adf14f62890a8cf449ad1d608de5c687ed30d12
Theo Godard <theo.godard@gmail.com>
2015-06-16 02:13:05 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -16,134 +16,139 @@ BaseAsset::register($this);
 ]); ?>
 
 <!-- MAIN CONTENT -->
-<h1 class="text-center">Accounting</h1>
-<div id="accounting-index">
-    <div class="row">
-
-        <!-- Balance Sheet -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="card informative-block transparent">
-                <div class="card-header">
-                    <div class="banner-title">
-                        <p>
-                            <i class="fa fa-pie-chart"></i>
-                            <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Balance Sheet</a>
-                        </p>
+<div class="fp-acc-page">
+    
+    <div class="header time-range">
+        <h1 class="text-center">Accounting</h1>
+    </div>
+    
+    <div id="accounting-index">
+        <div class="row">
+    
+            <!-- Balance Sheet -->
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card informative-block transparent">
+                    <div class="card-header">
+                        <div class="banner-title">
+                            <p>
+                                <i class="fa fa-pie-chart"></i>
+                                <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Balance Sheet</a>
+                            </p>
+                        </div>
+                        <div class="banner-subtitle">
+                            <p></p>
+                        </div>
                     </div>
-                    <div class="banner-subtitle">
-                        <p></p>
+                    <div id="accounting-balance-sheet-overview" class="card-content">
+                        <img src="/img/ajax-loader.gif">
                     </div>
-                </div>
-                <div id="accounting-balance-sheet-overview" class="card-content">
-                    <img src="/img/ajax-loader.gif">
                 </div>
             </div>
-        </div>
-
-        <!-- Income -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="card informative-block transparent">
-                <div class="card-header">
-                    <div class="banner-title">
-                        <p>
-                            <i class="fa fa-bar-chart"></i>
-                            <a href="<?= yii\helpers\Url::toRoute('/accounting/profitloss') ?>">Income</a>
-                        </p>
+    
+            <!-- Income -->
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card informative-block transparent">
+                    <div class="card-header">
+                        <div class="banner-title">
+                            <p>
+                                <i class="fa fa-bar-chart"></i>
+                                <a href="<?= yii\helpers\Url::toRoute('/accounting/profitloss') ?>">Income</a>
+                            </p>
+                        </div>
+                        <div class="banner-subtitle">
+                            <p></p>
+                        </div>
                     </div>
-                    <div class="banner-subtitle">
-                        <p></p>
+                    <div id="accounting-profit-loss-overview" class="card-content">
+                        <img src="/img/ajax-loader.gif">
                     </div>
-                </div>
-                <div id="accounting-profit-loss-overview" class="card-content">
-                    <img src="/img/ajax-loader.gif">
                 </div>
             </div>
-        </div>
-
-        <!-- Cash Flow -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="card informative-block transparent">
-                <div class="card-header">
-                    <div class="banner-title">
-                        <p>
-                            <i class="fa fa-random"></i>
-                            <a href="<?= yii\helpers\Url::toRoute('/accounting') ?>">Cash Flow</a>
-                        </p>
+    
+            <!-- Cash Flow -->
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card informative-block transparent">
+                    <div class="card-header">
+                        <div class="banner-title">
+                            <p>
+                                <i class="fa fa-random"></i>
+                                <a href="<?= yii\helpers\Url::toRoute('/accounting') ?>">Cash Flow</a>
+                            </p>
+                        </div>
+                        <div class="banner-subtitle">
+                            <p></p>
+                        </div>
                     </div>
-                    <div class="banner-subtitle">
-                        <p></p>
+                    <div id="accounting-cash-flow-overview" class="card-content">
+                        <img src="/img/ajax-loader.gif">
                     </div>
-                </div>
-                <div id="accounting-cash-flow-overview" class="card-content">
-                    <img src="/img/ajax-loader.gif">
                 </div>
             </div>
-        </div>
-  </div>
-  <div class="row">
-        <!-- Assets -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="card informative-block transparent">
-                <div class="card-header">
-                    <div class="banner-title">
-                        <p>
-                            <i class="fa fa-building-o"></i>
-                            <a href="<?= yii\helpers\Url::toRoute('/accounting/asset') ?>">Assets</a>
-                        </p>
+      </div>
+      <div class="row">
+            <!-- Assets -->
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card informative-block transparent">
+                    <div class="card-header">
+                        <div class="banner-title">
+                            <p>
+                                <i class="fa fa-building-o"></i>
+                                <a href="<?= yii\helpers\Url::toRoute('/accounting/asset') ?>">Assets</a>
+                            </p>
+                        </div>
+                        <div class="banner-subtitle">
+                            <p></p>
+                        </div>
                     </div>
-                    <div class="banner-subtitle">
-                        <p></p>
+                    <div id="accounting-assets-overview" class="card-content">
+                        <img src="/img/ajax-loader.gif">
                     </div>
-                </div>
-                <div id="accounting-assets-overview" class="card-content">
-                    <img src="/img/ajax-loader.gif">
                 </div>
             </div>
-        </div>
-
-        <!-- Operating Expenses -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="card informative-block transparent">
-                <div class="card-header">
-                    <div class="banner-title">
-                        <p>
-                            <i class="fa fa-shopping-cart"></i>
-                            <a href="<?= yii\helpers\Url::toRoute('/') ?>">Expenses</a></p>
-                        </p>
+    
+            <!-- Operating Expenses -->
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card informative-block transparent">
+                    <div class="card-header">
+                        <div class="banner-title">
+                            <p>
+                                <i class="fa fa-shopping-cart"></i>
+                                <a href="<?= yii\helpers\Url::toRoute('/') ?>">Expenses</a></p>
+                            </p>
+                        </div>
+                        <div class="banner-subtitle">
+                            <p></p>
+                        </div>
                     </div>
-                    <div class="banner-subtitle">
-                        <p></p>
+                    <div id="accounting-profit-loss-overview" class="card-content">
+                        <!--img src="/img/ajax-loader.gif"-->
                     </div>
-                </div>
-                <div id="accounting-profit-loss-overview" class="card-content">
-                    <!--img src="/img/ajax-loader.gif"-->
                 </div>
             </div>
-        </div>
-
-        <!-- Contracts -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="card informative-block transparent">
-                <div class="card-header">
-                    <div class="banner-title">
-                        <p>
-                            <i class="fa fa-file-text-o"></i>
-                            <a href="<?= yii\helpers\Url::toRoute('/') ?>">Contracts</a></p>
-                        </p>
+    
+            <!-- Contracts -->
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card informative-block transparent">
+                    <div class="card-header">
+                        <div class="banner-title">
+                            <p>
+                                <i class="fa fa-file-text-o"></i>
+                                <a href="<?= yii\helpers\Url::toRoute('/') ?>">Contracts</a></p>
+                            </p>
+                        </div>
+                        <div class="banner-subtitle">
+                            <p></p>
+                        </div>
                     </div>
-                    <div class="banner-subtitle">
-                        <p></p>
+                    <div id="accounting-cash-flow-overview" class="card-content">
+                        <!--img src="/img/ajax-loader.gif"-->
                     </div>
-                </div>
-                <div id="accounting-cash-flow-overview" class="card-content">
-                    <!--img src="/img/ajax-loader.gif"-->
                 </div>
             </div>
+            
         </div>
-        
     </div>
 </div>
-
 
 
 
