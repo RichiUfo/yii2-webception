0bcfd7876425718d32067674094bbea85b53b7e3
Theo Godard <theo.godard@gmail.com>
2015-06-16 12:14:33 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -1,1 +1,63 @@
-YO
+<div class="row">
+    
+    <!-- Balance Sheet -->
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+        <div class="card informative-block transparent">
+            <div class="card-header">
+                <div class="banner-title">
+                    <p>
+                        <i class="fa fa-pie-chart"></i>
+                        <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Balance Sheet</a>
+                    </p>
+                </div>
+                <div class="banner-subtitle">
+                    <p></p>
+                </div>
+            </div>
+            <div class="card-content">
+                
+            </div>
+        </div>
+    </div>
+    
+    <!-- Income -->
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+        <div class="card informative-block transparent">
+            <div class="card-header">
+                <div class="banner-title">
+                    <p>
+                        <i class="fa fa-pie-chart"></i>
+                        <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Profits &amp; Losses</a>
+                    </p>
+                </div>
+                <div class="banner-subtitle">
+                    <p></p>
+                </div>
+            </div>
+            <div class="card-content">
+                
+            </div>
+        </div>
+    </div>
+    
+    <!-- Cash Flow -->
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+        <div class="card informative-block transparent">
+            <div class="card-header">
+                <div class="banner-title">
+                    <p>
+                        <i class="fa fa-pie-chart"></i>
+                        <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Cash Flow</a>
+                    </p>
+                </div>
+                <div class="banner-subtitle">
+                    <p></p>
+                </div>
+            </div>
+            <div class="card-content">
+                
+            </div>
+        </div>
+    </div>
+    
+</div>
