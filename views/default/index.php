ea1c4a1ae53bd1b77f7b7905caca7b72679ee63f
Theo Godard <theo.godard@gmail.com>
2015-06-16 02:23:02 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -19,23 +19,28 @@ BaseAsset::register($this);
 <!-- MAIN CONTENT -->
 <div class="fp-acc-page">
     
-    <div class="header time-range">
-        <h1 class="text-center">Accounting</h1>
-        <div id="account-period-selection" class="right-col-item" style="margin: 10px">
-            <?= DateRangePicker::widget([
-                'name' => 'date_from',
-                'size' => 'sm',
-                'value' => date("Y-m-d", strtotime(date("Y-m-d").' -1 months')),
-                'nameTo' => 'name_to',
-                'valueTo' => date("Y-m-d"),
-                'clientOptions' => [
-                    'format' => 'yyyy-mm-dd',
-                    'autoclose' => true
-                ],
-                'clientEvents' => [
-                    'changeDate' => 'function ev(){timePeriodChangeHandler();}'
-                ]
-            ]); ?>
+    <div class="container-fluid header time-range">
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h1 class="text-center">Accounting</h1>
+            </div>
+            <div class="col-lg-6">
+                <?= DateRangePicker::widget([
+                    'name' => 'date_from',
+                    'size' => 'sm',
+                    'value' => date("Y-m-d", strtotime(date("Y-m-d").' -1 months')),
+                    'nameTo' => 'name_to',
+                    'valueTo' => date("Y-m-d"),
+                    'clientOptions' => [
+                        'format' => 'yyyy-mm-dd',
+                        'autoclose' => true
+                    ],
+                    'clientEvents' => [
+                        'changeDate' => 'function ev(){timePeriodChangeHandler();}'
+                    ]
+                ]); ?>
+            </div>
         </div>
     </div>
     
