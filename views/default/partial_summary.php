434485ac98ed8ab1cdb031c9001c931502d736af
Theo Godard <theo.godard@gmail.com>
2015-06-17 02:15:46 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -4,7 +4,7 @@
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
         <div class="card informative-block transparent">
             <div class="card-header">
-                <div class="banner-title h2">
+                <div class="banner-title h2 text-center">
                     Balance Sheet
                     <!--p>
                         <i class="fa fa-pie-chart"></i>
