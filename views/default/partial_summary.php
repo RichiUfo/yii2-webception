bdc6620064ef5f5aa2d40db41ecbfd1fa27cb95e
Theo Godard <theo.godard@gmail.com>
2015-06-17 23:38:41 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -14,7 +14,7 @@ use frontend\widgets\chartjs\ChartJs;
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
        <h2 class="text-center">Profits &amp; Losses</h2>
        
-        <?= \frontend\widgets\chartjs\ChartJs::widget([
+        <?= ChartJs::widget([
             'type' => 'Line',
             'clientOptions' => [
                 'showScale' => false,
