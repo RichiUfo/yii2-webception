33a40f668033f8f3b0d83c59d6f03c93b86cfa24
Theo Godard <theo.godard@gmail.com>
2015-06-17 02:19:04 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -3,47 +3,16 @@
     <!-- Balance Sheet -->
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
         <h2 class="text-center">Balance Sheet</h2>
-        
     </div>
     
     <!-- Income -->
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-        <div class="card informative-block transparent">
-            <div class="card-header">
-                <div class="banner-title">
-                    <p> 
-                        <i class="fa fa-pie-chart"></i>
-                        <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Profits &amp; Losses</a>
-                    </p>
-                </div>
-                <div class="banner-subtitle">
-                    <p></p>
-                </div>
-            </div>
-            <div class="card-content">
-                
-            </div>
-        </div>
+       <h2 class="text-center">Profits &amp; Losses</h2>
     </div>
     
     <!-- Cash Flow -->
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-        <div class="card informative-block transparent">
-            <div class="card-header">
-                <div class="banner-title">
-                    <p>
-                        <i class="fa fa-pie-chart"></i>
-                        <a href="<?= yii\helpers\Url::toRoute('/accounting/balancesheet') ?>">Cash Flow</a>
-                    </p>
-                </div>
-                <div class="banner-subtitle">
-                    <p></p>
-                </div>
-            </div>
-            <div class="card-content">
-                
-            </div>
-        </div>
+        <h2 class="text-center">Cash Flow</h2>
     </div>
     
 </div>
