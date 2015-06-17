9441df7ae36c2da84f12f27080ee514c850450d7
Theo Godard <theo.godard@gmail.com>
2015-06-17 02:57:30 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -7,33 +7,7 @@ use dosamigos\chartjs\ChartJs;
     <!-- Balance Sheet -->
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
         <h2 class="text-center">Balance Sheet</h2>
-        <?= ChartJs::widget([
-            'type' => 'Line',
-            'options' => [
-                'height' => 400,
-                'width' => 400
-            ],
-            'data' => [
-                'labels' => ["January", "February", "March", "April", "May", "June", "July"],
-                'datasets' => [
-                    [
-                        'fillColor' => "rgba(220,220,220,0.5)",
-                        'strokeColor' => "rgba(220,220,220,1)",
-                        'pointColor' => "rgba(220,220,220,1)",
-                        'pointStrokeColor' => "#fff",
-                        'data' => [65, 59, 90, 81, 56, 55, 40]
-                    ],
-                    [
-                        'fillColor' => "rgba(151,187,205,0.5)",
-                        'strokeColor' => "rgba(151,187,205,1)",
-                        'pointColor' => "rgba(151,187,205,1)",
-                        'pointStrokeColor' => "#fff",
-                        'data' => [28, 48, 40, 19, 96, 27, 100]
-                    ]
-                ]
-            ]
-        ]);
-        ?>
+        
     </div>
     
     <!-- Income -->
