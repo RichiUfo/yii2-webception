e8d450a9ca07941d462f3f2f1c6b0eb52790bd6a
Theo Godard <theo.godard@gmail.com>
2015-06-17 02:53:57 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -39,6 +39,33 @@ use dosamigos\chartjs\ChartJs;
     <!-- Income -->
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
        <h2 class="text-center">Profits &amp; Losses</h2>
+       <?= ChartJs::widget([
+            'type' => 'Line',
+            'options' => [
+                'height' => 250,
+                'width' => 100%
+            ],
+            'data' => [
+                'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                'datasets' => [
+                    [
+                        'fillColor' => "rgba(220,220,220,0.5)",
+                        'strokeColor' => "rgba(220,220,220,1)",
+                        'pointColor' => "rgba(220,220,220,1)",
+                        'pointStrokeColor' => "#fff",
+                        'data' => [65, 59, 90, 81, 56, 55, 40]
+                    ],
+                    [
+                        'fillColor' => "rgba(151,187,205,0.5)",
+                        'strokeColor' => "rgba(151,187,205,1)",
+                        'pointColor' => "rgba(151,187,205,1)",
+                        'pointStrokeColor' => "#fff",
+                        'data' => [28, 48, 40, 19, 96, 27, 100]
+                    ]
+                ]
+            ]
+        ]);
+        ?>
     </div>
     
     <!-- Cash Flow -->
