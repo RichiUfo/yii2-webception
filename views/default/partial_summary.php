eeab6131bf71439718ca757478875ceb5ea94421
Theo Godard <theo.godard@gmail.com>
2015-06-17 07:11:21 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -16,6 +16,7 @@ use dosamigos\chartjs\ChartJs;
        <?= ChartJs::widget([
             'type' => 'Line',
             'clientOptions' => [
+                'showScale' => false,
                 'scaleShowGridLines' => false,
                 'scaleShowLabels' => false,
                 'responsive' => true,
