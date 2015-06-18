dbcf5618c9a260dd847a278e42227b4280e78619
Theo Godard <theo.godard@gmail.com>
2015-06-18 05:50:10 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -45,19 +45,19 @@ use frontend\widgets\chartjs\ChartJs;
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-12">
                 <div class="row">
-                    <div class="col-xs-4">
+                    <div class="col-xs-4 col-sm-12 col-md-12 col-lg-4">
                         <div class="data-block">
                             <span class="data-block-value money" value="<?= $data['total_assets'] ?>" currency="" decimal="0"></span>
                             <span class="data-block-title">Assets</span>
                         </div>
                     </div>
-                    <div class="col-xs-4">
+                    <div class="col-xs-4 col-sm-12 col-md-12 col-lg-4">
                         <div class="data-block">
                             <span class="data-block-value money" value="<?= $data['total_liabilities'] ?>" currency="" decimal="0"></span>
                             <span class="data-block-title">Debt</span>
                         </div>
                     </div>
-                    <div class="col-xs-4">
+                    <div class="col-xs-4 col-sm-12 col-md-12 col-lg-4">
                         <div class="data-block">
                             <span class="data-block-value money" value="<?= $data['total_equity'] ?>" currency="" decimal="0"></span>
                             <span class="data-block-title">Net Worth</span>
