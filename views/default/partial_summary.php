4d3a83fba778a2d8ce1980ec9cc4dcfcb2abc25d
Theo Godard <theo.godard@gmail.com>
2015-06-18 03:12:15 +0000

    Autocommit

--- views/default/partial_summary.php
+++ views/default/partial_summary.php
@@ -66,23 +66,17 @@ use frontend\widgets\chartjs\ChartJs;
                     [
                         'fillColor' => "rgba(41,171,164,0.5)",
                         'strokeColor' => "rgb(41,171,164)",
-                        //'pointColor' => "rgba(220,220,220,1)",
-                        //'pointStrokeColor' => "#fff",
                         'data' => [0, 59, 90, 81, 56, 55, 90]
-                    ],
-                    [
-                        'fillColor' => "#fff",
-                        'strokeColor' => "rgb(235,114,96)",
-                        //'pointColor' => "rgba(151,187,205,1)",
-                        //'pointStrokeColor' => "#fff",
-                        'data' => [0, -48, -40, -19, -96, -27, -100]
                     ],
                     [
                         'fillColor' => "rgba(58,154,217,0.5)",
                         'strokeColor' => "rgb(58,154,217)",
-                        //'pointColor' => "rgba(151,187,205,1)",
-                        //'pointStrokeColor' => "#fff",
                         'data' => [0, 5, 4, 10, 20, 27, 50]
+                    ],
+                    [
+                        'fillColor' => "#FEFEFE",
+                        'strokeColor' => "rgb(235,114,96)",
+                        'data' => [0, -48, -40, -19, -96, -27, -100]
                     ]
                 ]
             ]
