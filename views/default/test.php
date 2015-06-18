8c5609c44be558a34d9bdac5fc5e8957f2598b5e
Theo Godard <theo.godard@gmail.com>
2015-06-18 11:33:45 +0000

    Autocommit

--- views/default/test.php
+++ views/default/test.php
@@ -1,12 +1,14 @@
-<?php var_dump($histo); ?>
-
-<table class="table table-striped">
-<tbody>
-<?php foreach($histo as $date => $val) : ?>
-<tr>
-    <td><?= $date ?></td>
-    <td><?= $val ?></td>
-</tr>
-<?php endforeach; ?>
-</tbody>
-</table>
+<div class="row">
+    <div class="col-lg-6">
+        <table class="table table-striped">
+        <tbody>
+        <?php foreach($histo as $date => $val) : ?>
+        <tr>
+            <td><?= $date ?></td>
+            <td><?= $val ?></td>
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+        </table>
+    </div>
+</div>
