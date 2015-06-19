2781e5b08c14187a55012e47cf7659679a50c71b
Theo Godard <theo.godard@gmail.com>
2015-06-19 02:42:11 +0000

    Autocommit

--- views/default/test.php
+++ views/default/test.php
@@ -1,4 +1,32 @@
 <h1>Current Value= <?= $value ?></h1>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1>Multiple currencies histo valued</h1>
+        <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>T/C</th>
+                <?php foreach($currencies as $cur) : ?>
+                <th><?= $cur ?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>    
+        <tbody>
+            <?php foreach($histos as $date => $vals) : ?>
+            <tr>
+                <td><?= $date ?></td>
+                <?php foreach($currencies as $cur) : ?>
+                <td><?= $vals[$cur] ?></td>
+                <?php endforeach; ?>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+        </table>
+    </div>
+</div>
+
+
 <div class="row">
     <div class="col-lg-6">
         <h1>Historical Values</h1>
@@ -26,31 +54,6 @@
             </tr>
             <?php endforeach; ?>
             </tbody>
-        </table>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12">
-        <h1>Multiple currencies histo valued</h1>
-        <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>T/C</th>
-                <?php foreach($currencies as $cur) : ?>
-                <th><?= $cur ?></th>
-                <?php endforeach; ?>
-            </tr>
-        </thead>    
-        <tbody>
-            <?php foreach($histos as $date => $vals) : ?>
-            <tr>
-                <td><?= $date ?></td>
-                <?php foreach($currencies as $cur) : ?>
-                <td><?= $vals[$cur] ?></td>
-                <?php endforeach; ?>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
         </table>
     </div>
 </div>
