445c844d6999e236a48f6865f00035ce52a890a5
Theo Godard <theo.godard@gmail.com>
2015-06-19 07:43:33 +0000

    Autocommit

--- controllers/TransactionController.php
+++ controllers/TransactionController.php
@@ -152,7 +152,6 @@ class TransactionController extends \frontend\components\Controller
             $transaction->credit = (in_array($transaction->account_credit_id, $ids))?true:false;       
             $transaction->debit = (in_array($transaction->account_debit_id, $ids))?true:false;    
             $transaction->currency = $transaction->accountCredit->currency;
-            $transaction->transactionForex;
         }
         
         return $transactions;
