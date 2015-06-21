2eb5682e153d759db1af6ba62ad152a5abde48db
Theo Godard <theo.godard@gmail.com>
2015-06-21 16:59:22 +0000

    Autocommit

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -319,6 +319,26 @@ class AccountController extends \frontend\components\Controller
         
         // Sort & Return the values
         ksort($datapoints);
+        
+        // Calculate the total in system currency 
+        $currency = 'EUR';
+        foreach($datapoints as $date => $datapoint) {
+            $total = 0;
+            foreach($datapoint as $cur => $val) {
+                if($cur !== $currency) {
+                    $total += ExchangeController::get('finance', 'currency-conversion', [
+                        'value' => $val,
+                        'from' => $cur,
+                        'to' => $currency
+                    ]);
+                }
+                else {
+                    $total += $val;
+                }
+            }
+            $datapoint['total'] = $total;
+        }
+        
         return $datapoints;
     }
     public function getAccountCurrencies($accountid) {
