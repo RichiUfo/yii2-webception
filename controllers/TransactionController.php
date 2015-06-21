7474345a5f4cdafd41773f6f040f8de74225bc63
Theo Godard <theo.godard@gmail.com>
2015-06-21 16:05:40 +0000

    Autocommit

--- controllers/TransactionController.php
+++ controllers/TransactionController.php
@@ -136,7 +136,7 @@ class TransactionController extends \frontend\components\Controller
         
         // Find the transactions
         $transactions = Transaction::find()
-            ->joinWith(['transactionForex', 'accountForex'])
+            ->joinWith(['accountForex'])
             ->where('account_credit_id IN '.$strids.' OR account_debit_id IN '.$strids.' OR transactions_forex.account_forex_id IN '.$strids)
             ->andWhere($time_query)
             ->orderBy(['date_value' => SORT_DESC])
