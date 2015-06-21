2911525a663fdd41d60f2d9bb20b3cc4d86842d7
Theo Godard <theo.godard@gmail.com>
2015-06-21 06:04:42 +0000

    Autocommit

--- views/default/test.php
+++ views/default/test.php
@@ -37,7 +37,7 @@
             <tr>
                 <td><?= $t->date_value ?></td>
                 <td><?= $t->value ?></td>
-                <td><?php var_dump($t->transactionForex); ?></td>
+                <td><?php var_dump($t->transactionForex['id']); ?></td>
             </tr>
             <?php endforeach; ?>
             </tbody>
