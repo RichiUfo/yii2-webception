623dd13c456b382e2fc2e32ea1f283c98642c6bd
Theo Godard <theo.godard@gmail.com>
2015-06-21 17:14:41 +0000

    Autocommit

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -336,7 +336,7 @@ class AccountController extends \frontend\components\Controller
                     $total += $val;
                 }
             }
-            $datapoints[$date]['total'] = $total;
+            $datapoints[$date]['total'] = round($total, 2;
         }
         
         return $datapoints;
