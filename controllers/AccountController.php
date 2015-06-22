6a64fb0854d16160380f98ce6496d7934964296d
Theo Godard <theo.godard@gmail.com>
2015-06-22 12:19:27 +0000

    Autocommit

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -291,15 +291,13 @@ class AccountController extends \frontend\components\Controller
         $currency = 'EUR';
         foreach($datapoints as $date => $datapoint) {
             $total = 0;
-            $date_dt_temp = new \DateTime($date);
-            $date_dt_temp->sub( \DateInterval::createFromDateString('1 day') );
             foreach($datapoint as $cur => $val) {
                 if($cur !== $currency) {
                     $total += ExchangeController::get('finance', 'currency-conversion', [
                         'value' => $val,
                         'from' => $cur,
                         'to' => $currency,
-                        'date' => $date
+                        'date' => null
                     ]);
                 }
                 else {
