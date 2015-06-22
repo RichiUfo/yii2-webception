5db9849d7e3007eba2ce022f5f6e8af32d41d3f4
Theo Godard <theo.godard@gmail.com>
2015-06-22 05:38:15 +0000

    Autocommit

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -71,7 +71,7 @@ class DefaultController extends \frontend\components\Controller
 	
 	public function actionTest($id, $s, $e) { 
 		
-		$value = AccountController::getCurrentAccountValue($id, 'EUR');
+		//$value = AccountController::getCurrentAccountValue($id, 'EUR');
 		$values = AccountController::getCurrentBalances($id);
 		$currencies = AccountController::getAccountCurrencies($id);
 		$trans = TransactionController::getTransactions($id, $s, $e);
