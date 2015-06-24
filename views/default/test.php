b01a366f6088313502f562a065311c27f2ad3219
Theo Godard <theo.godard@gmail.com>
2015-06-24 23:53:37 +0000

    Autocommit

--- views/default/test.php
+++ views/default/test.php
@@ -1,8 +1,8 @@
-<h1 class="text-center">Historical Account Value </h1>
+<h1 class="text-center">Historical Account Value  (<?= $value ?>)</h1>
 
 <div class="row">
     <div class="col-lg-12">
-        <h1>Historical Values (<?= $value ?>)</h1>
+        <h1>Historical Values</h1>
         <table class="table table-striped">
         <thead>
             <tr>
