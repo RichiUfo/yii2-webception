9c196d5a4f8d3e3229d7a0513ebae58b2e2c0f1e
Theo Godard <theo.godard@gmail.com>
2015-06-26 02:45:57 +0000

    Autocommit

--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -10,7 +10,51 @@
             
             <!-- Movements -->
             <div id="movements-summary-ajax" class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-
+                <h2>Movements</h2>
+                <table style="width: 100%">
+                    <tr>
+                        <td>Opening Balance</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $account->sign*$movements['opening_balance'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Debits</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $movements['passed_debits'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Credits</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $movements['passed_credits'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Current Balance</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $account->sign*$movements['current_balance'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Future Debits</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $movements['future_debits'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Future Credits</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $movements['future_credits'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Closing Balance</td>
+                        <td class="text-right">
+                            <span class="money" value="<?= $account->sign*$movements['closing_balance'] ?>" currency=""></span>
+                        </td>
+                    </tr>
+                </table>
             </div>
             
             <!-- Children -->
--- views/account/partial_movements.php
+++ views/account/partial_movements.php
@@ -5,58 +5,48 @@ $end_dt = new \DateTime($end);
 $period = $start_dt->format('M j').' - '.$end_dt->format('M j');
 ?>
 <div class="card informative-block">
-    <div class="card-header">
-        <div class="banner-title">
-            <p>Movements</p>
-        </div>
-        <div class="banner-subtitle">
-            <p><?= $period ?></p>
-        </div>
-    </div>
-    <div class="card-content">
-        <table style="width: 100%">
-            <tr>
-                <td>Opening Balance</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $account->sign*$movements['opening_balance'] ?>" currency=""></span>
-                </td>
-            </tr>
-            <tr>
-                <td>Debits</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $movements['passed_debits'] ?>" currency=""></span>
-                </td>
-            </tr>
-            <tr>
-                <td>Credits</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $movements['passed_credits'] ?>" currency=""></span>
-                </td>
-            </tr>
-            <tr>
-                <td>Current Balance</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $account->sign*$movements['current_balance'] ?>" currency=""></span>
-                </td>
-            </tr>
-            <tr>
-                <td>Future Debits</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $movements['future_debits'] ?>" currency=""></span>
-                </td>
-            </tr>
-            <tr>
-                <td>Future Credits</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $movements['future_credits'] ?>" currency=""></span>
-                </td>
-            </tr>
-            <tr>
-                <td>Closing Balance</td>
-                <td class="text-right">
-                    <span class="money" value="<?= $account->sign*$movements['closing_balance'] ?>" currency=""></span>
-                </td>
-            </tr>
-        </table>
-    </div>
-</div>
+    <h2>Movements</h2>
+    <table style="width: 100%">
+        <tr>
+            <td>Opening Balance</td>
+            <td class="text-right">
+                <span class="money" value="<?= $account->sign*$movements['opening_balance'] ?>" currency=""></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Debits</td>
+            <td class="text-right">
+                <span class="money" value="<?= $movements['passed_debits'] ?>" currency=""></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Credits</td>
+            <td class="text-right">
+                <span class="money" value="<?= $movements['passed_credits'] ?>" currency=""></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Current Balance</td>
+            <td class="text-right">
+                <span class="money" value="<?= $account->sign*$movements['current_balance'] ?>" currency=""></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Future Debits</td>
+            <td class="text-right">
+                <span class="money" value="<?= $movements['future_debits'] ?>" currency=""></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Future Credits</td>
+            <td class="text-right">
+                <span class="money" value="<?= $movements['future_credits'] ?>" currency=""></span>
+            </td>
+        </tr>
+        <tr>
+            <td>Closing Balance</td>
+            <td class="text-right">
+                <span class="money" value="<?= $account->sign*$movements['closing_balance'] ?>" currency=""></span>
+            </td>
+        </tr>
+    </table>
