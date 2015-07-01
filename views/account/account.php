c0d39588dc10923222e380c7f8ea427a63abeca4
Theo Godard <theo.godard@gmail.com>
2015-07-01 12:55:46 +0000

    Autocommit

--- views/account/account.php
+++ views/account/account.php
@@ -3,6 +3,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use dosamigos\datepicker\DateRangePicker;
 use frontend\widgets\editable\Editable;
+use \fruppel\googlecharts\GoogleCharts;
 
 use frontend\modules\accounting\assets\BaseAsset;
 BaseAsset::register($this);
--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -1,7 +1,3 @@
-<?php
-use \fruppel\googlecharts\GoogleCharts;
-?>
-
 <div id="account-display">
     
     <div class="container-fluid">
@@ -10,62 +6,62 @@ use \fruppel\googlecharts\GoogleCharts;
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
                 <h2>Overview</h2>
                 <?= GoogleCharts::widget([
-                'id' => 'my-id',
-                'visualization' => 'PieChart',
-                'data' => [
-                    'cols' => [
-                        [
-                            'id' => 'topping',
-                            'label' => 'Topping',
-                            'type' => 'string'
+                    'id' => 'my-id',
+                    'visualization' => 'PieChart',
+                    'data' => [
+                        'cols' => [
+                            [
+                                'id' => 'topping',
+                                'label' => 'Topping',
+                                'type' => 'string'
+                            ],
+                            [
+                                'id' => 'slices',
+                                'label' => 'Slices',
+                                'type' => 'number'
+                            ]
                         ],
-                        [
-                            'id' => 'slices',
-                            'label' => 'Slices',
-                            'type' => 'number'
+                        'rows' => [
+                            [
+                                'c' => [
+                                    ['v' => 'Mushrooms'],
+                                    ['v' => 3]
+                                ],
+                            ],
+                            [
+                                'c' => [
+                                    ['v' => 'Onions'],
+                                    ['v' => 1]
+                                ]
+                            ],
+                            [
+                                'c' => [
+                                    ['v' => 'Olives'],
+                                    ['v' => 1]
+                                ]
+                            ],
+                            [
+                                'c' => [
+                                    ['v' => 'Zucchini'],
+                                    ['v' => 1]
+                                ]
+                            ],
+                            [
+                                'c' => [
+                                    ['v' => 'Pepperoni'],
+                                    ['v' => 2]
+                                ]
+                            ],
                         ]
                     ],
-                    'rows' => [
-                        [
-                            'c' => [
-                                ['v' => 'Mushrooms'],
-                                ['v' => 3]
-                            ],
-                        ],
-                        [
-                            'c' => [
-                                ['v' => 'Onions'],
-                                ['v' => 1]
-                            ]
-                        ],
-                        [
-                            'c' => [
-                                ['v' => 'Olives'],
-                                ['v' => 1]
-                            ]
-                        ],
-                        [
-                            'c' => [
-                                ['v' => 'Zucchini'],
-                                ['v' => 1]
-                            ]
-                        ],
-                        [
-                            'c' => [
-                                ['v' => 'Pepperoni'],
-                                ['v' => 2]
-                            ]
-                        ],
-                    ]
-                ],
-                'options' => [
-                    'title' => 'How Much Pizza I Ate Last Night',
-                    'width' => 400,
-                    'height' => 300,
-                    'is3D' => true,
-                ],
-                'responsive' => true,
-            ]) ?>
+                    'options' => [
+                        'title' => 'How Much Pizza I Ate Last Night',
+                        'width' => 400,
+                        'height' => 300,
+                        'is3D' => true,
+                    ],
+                    'responsive' => true,
+                ]) ?>
             </div>
             
             <!-- Movements -->
