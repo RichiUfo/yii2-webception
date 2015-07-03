ba7306d5aae67d9014e1ce8b5b36c5c84b0954f0
Theo Godard <theo.godard@gmail.com>
2015-07-03 03:51:01 +0000

    Autocommit

--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -33,8 +33,8 @@
                         <div class="icon-container">
                             <i class="fa fa-arrow-right"></i>
                         </div>
-                        <svg height="50" width="2" style="position:absolute;right:0;bottom:0;height:50%;z-index:1">
-                            <line x1="0" y1="0" x2="0" y2="49" style="stroke:rgb(231,234,236);stroke-width:2" />
+                        <svg height="50" width="1" style="position:absolute;right:0;bottom:0;height:50%;z-index:1">
+                            <line x1="0" y1="0" x2="0" y2="49" style="stroke:rgb(231,234,236);stroke-width:1" />
                         </svg>
                     </div>
                     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -45,8 +45,8 @@
                         <div class="icon-container">
                             <i class="fa fa-arrow-right"></i>
                         </div>
-                        <svg height="50" width="2" style="position:absolute;right:0;bottom:0;height:50%;z-index:1">
-                            <line x1="0" y1="0" x2="0" y2="49" style="stroke:rgb(231,234,236);stroke-width:2" />
+                        <svg height="50" width="1" style="position:absolute;right:0;bottom:0;height:50%;z-index:1">
+                            <line x1="0" y1="0" x2="0" y2="49" style="stroke:rgb(231,234,236);stroke-width:1" />
                         </svg>
                     </div>
                     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
