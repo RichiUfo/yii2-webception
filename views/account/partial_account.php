7230b245fa5f746fa4f0be62e4458a7055f38a74
Theo Godard <theo.godard@gmail.com>
2015-07-03 03:17:15 +0000

    Autocommit

--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -42,7 +42,7 @@
                         <div class="icon-container">
                             <i class="fa fa-arrow-right"></i>
                         </div>
-                        <svg height="50" width="1" style="position:absolute;right:0;height:50%">
+                        <svg height="50" width="1" style="position:absolute;right:0;bottom:0;height:50%;z-index:1">
                             <line x1="0" y1="0" x2="0" y2="49" style="stroke:rgb(255,0,0);stroke-width:1" />
                         </svg>
                     </div>
