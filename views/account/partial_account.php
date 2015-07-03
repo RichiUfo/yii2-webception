0933693e28aa95b2b867ee7e5b4b7d5ceb0b3f71
Theo Godard <theo.godard@gmail.com>
2015-07-03 03:23:11 +0000

    Autocommit

--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -54,6 +54,19 @@
                     </div>
                 </div>
                 
+                <div class="row overview-row movements">
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div class="movements-container">
+                            content
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div class="movements-container">
+                            content
+                        </div>
+                    </div>
+                </div>
+                
                 <!--div class="row overview-row movements">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-total">
                         <div class="data-block">
