37108e51c64d3e1b6f1af3928732e9264edc4959
Theo Godard <theo.godard@gmail.com>
2015-07-03 03:56:15 +0000

    Autocommit

--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -63,7 +63,7 @@
                             content
                         </div>
                     </div>
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                         <div class="movements-container">
                             content
                         </div>
