e22e2122ba09781fc950ec72e435466908dabda6
Theo Godard <theo.godard@gmail.com>
2015-07-06 06:09:15 +0000

    Autocommit

--- views/profitloss/partial_profitloss.php
+++ views/profitloss/partial_profitloss.php
@@ -31,26 +31,29 @@ function displayHierarchy($accounts){
 
 ?>
 
-
 <div id="profit-loss">
     <div class="row">
         <div class="col-lg-6">
+            
             <!-- P&L SECTION -->
-            <h1>
-                <a href="">Operating Profit</a>
-                <span class="pull-right">
-                    <span class="money" 
-                          value="<?= $operating_revenues->sign * $operating_revenues->value_converted ?>" 
-                          currency="<?= $operating_revenues->currency ?> ">
-                    </span>
-                </span>
-            </h1>
-        
-            <div class="row">
-                <div class="col-lg-12 col-same-height col-middle">
+            <div class="card informative-block transparent">
+                <div class="card-header">
+                    <div class="banner-title">
+                        <p><a href="<?php echo Url::to(['account/display', 'id' => $assets->id]) ?>">Operating Profit</a></p>
+                    </div>
+                    <div class="banner-subtitle">
+                        <p>
+                            <span   class="money" 
+                                    value="<?= $operating_revenues->sign * $operating_revenues->value_converted ?>" 
+                                    currency="<?= $operating_revenues->currency ?> ">
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="card-content">
                     <?php displayHierarchy(array($operating_revenues, $operating_expenses)); ?>
                 </div>
-            </div> 
+            </div>
         </div>
 
         <div class="col-lg-6">
