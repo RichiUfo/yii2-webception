77e74431b62b6e4783b045916dd5173a25ddde48
Theo Godard <theo.godard@gmail.com>
2015-07-06 03:33:09 +0000

    Autocommit

--- views/account/partial_account.php
+++ views/account/partial_account.php
@@ -17,7 +17,7 @@ use yii\helpers\Url;
                             <span class="data-block-title">Opening</span>
                         </div>
                         <div class="icon-container">
-                            <i class="fa fa-level-up"></i>
+                            <i class="fa fa-arrow-right"></i>
                         </div>
                         <svg height="50" width="1" style="position:absolute;right:0;bottom:0;height:50%;z-index:1">
                             <line x1="0" y1="0" x2="0" y2="49" style="stroke:rgb(231,234,236);stroke-width:1" />
