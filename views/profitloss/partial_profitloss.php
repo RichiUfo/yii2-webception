c24775b81209fcbaf2860218de014b3967c2e85e
Theo Godard <theo.godard@gmail.com>
2015-07-06 05:11:45 +0000

    Autocommit

--- views/profitloss/partial_profitloss.php
+++ views/profitloss/partial_profitloss.php
@@ -36,70 +36,38 @@ function displayHierarchy($accounts){
     <div class="row">
         <div class="col-lg-6">
             <!-- P&L SECTION -->
-            <div class="godard-box">
-                <div class="godard-box-header">
-                    <div class="godard-box-icon">
-                        <i class="fa fa-briefcase"></i>
-                    </div>
-                    <div class="godard-box-title">
-                        <h1>
-                            <a href="">Operating Profit</a>
-                            <span class="pull-right">
-                                <span class="money" 
-                                      value="<?= $operating_revenues->sign * $operating_revenues->value_converted ?>" 
-                                      currency="<?= $operating_revenues->currency ?> ">
-                                </span>
-                            </span>
-                        </h1>
-                    </div>
+            <h1>
+                <a href="">Operating Profit</a>
+                <span class="pull-right">
+                    <span class="money" 
+                          value="<?= $operating_revenues->sign * $operating_revenues->value_converted ?>" 
+                          currency="<?= $operating_revenues->currency ?> ">
+                    </span>
+                </span>
+            </h1>
+        
+            <div class="row">
+                <div class="col-lg-12 col-same-height col-middle">
+                    <?php displayHierarchy(array($operating_revenues, $operating_expenses)); ?>
                 </div>
-                <div class="godard-box-secondrow">
-                    <div class="godard-box-filler">&nbsp;</div>
-                    <div class="godard-box-content">
-
-                        <div class="row">
-                            <div class="row-same-height">
-                                <div class="col-lg-12 col-same-height col-middle">
-                                    <?php displayHierarchy(array($operating_revenues, $operating_expenses)); ?>
-                                </div>
-                            </div>
-                        </div> 
-                    </div>
-                </div>
-            </div>
+            </div> 
         </div>
 
         <div class="col-lg-6">
             <!-- BALANCE SHEET SECTION -->
-            <div class="godard-box">
-                <div class="godard-box-header">
-                    <div class="godard-box-icon">
-                        <i class="fa fa-university"></i>
-                    </div>
-                    <div class="godard-box-title">
-                        <h1>
-                            <a href="">Non-Operating Profit</a>
-                            <span class="pull-right">
-                                <span class="money" 
-                                      value="<?= $non_operating_revenues->sign * $non_operating_revenues->value_converted ?>" 
-                                      currency="<?= $non_operating_revenues->currency ?> ">
-                                </span>
-                            </span>
-                        </h1>
-                    </div>
-                </div>
-                <div class="godard-box-secondrow">
-                    <div class="godard-box-filler">&nbsp;</div>
-                    <div class="godard-box-content">
-
-                        <div class="row">
-                            <div class="row-same-height">
-                                <div class="col-lg-12 col-same-height col-middle">
-                                    <?php displayHierarchy(array($non_operating_revenues, $non_operating_expenses)); ?>
-                                </div>
-                            </div>
-                        </div> 
-                    </div>
+            <h1>
+                <a href="">Non-Operating Profit</a>
+                <span class="pull-right">
+                    <span class="money" 
+                          value="<?= $non_operating_revenues->sign * $non_operating_revenues->value_converted ?>" 
+                          currency="<?= $non_operating_revenues->currency ?> ">
+                    </span>
+                </span>
+            </h1>
+        
+            <div class="row">
+                <div class="col-lg-12 col-same-height col-middle">
+                    <?php displayHierarchy(array($non_operating_revenues, $non_operating_expenses)); ?>
                 </div>
             </div>
         </div>
