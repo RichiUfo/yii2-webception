71192163996f530c5f1e18f4aadc33dbe5e55c1e
Theo Godard <theo.godard@gmail.com>
2015-07-15 02:11:18 +0000

    Autocommit

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -412,7 +412,7 @@ class AccountController extends \frontend\components\Controller
         }
         
         // Sort & Return the values
-        ksort($datapoints);
+        if ($datapoints) ksort($datapoints);
         return $datapoints;
     }
     public function getHistoricalBalance($accountid, $start, $end, $currency = null, $extrapolate = null) {
