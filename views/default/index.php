bd7f68dc941e55e1d3e1078c641f2eea068d4968
Theo Godard <theo.godard@gmail.com>
2015-07-16 03:44:58 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -110,7 +110,35 @@ BaseAsset::register($this);
     
     <!-- Quick Actions -->
     <div class="col-lg-4">
-        
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th colspan="3">Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Debt To Equity</td>
+                    <td class="fit-width"><span class="pull-right">47 %</span></td>
+                    <td class="text-right fit-width text-green"><i class="material-icons">trending_up</i><span class="money" value="125.12" currency=""></span></td>
+                </tr>
+                <tr>
+                    <td>Current Ratio</td>
+                    <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
+                    <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                </tr>
+                <tr>
+                    <td>Return On Assets</td>
+                    <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
+                    <td class="text-right fit-width text-red"><i class="material-icons">trending_down</i><span class="money" value="125.12" currency=""></span></td>
+                </tr>
+                <tr>
+                    <td>Return On Equity</td>
+                    <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
+                    <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
     
 <?php $this->endBlock(); ?>
