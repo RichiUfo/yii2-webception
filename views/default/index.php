709648573d7634a6eb4f1d3b52178b17a86b7f4c
Theo Godard <theo.godard@gmail.com>
2015-07-16 02:31:42 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -46,7 +46,7 @@ BaseAsset::register($this);
             <tr>
                 <td>Assets</td>
                 <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">trending_up</i><span class="money" value="125.12" currency=""></span></td>
             </tr>
             <tr>
                 <td>Assets</td>
@@ -56,7 +56,7 @@ BaseAsset::register($this);
             <tr>
                 <td>Assets</td>
                 <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                <td class="text-right fit-width text-red"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                <td class="text-right fit-width text-red"><i class="material-icons">trending_down</i><span class="money" value="125.12" currency=""></span></td>
             </tr>
             <tr>
                 <td>Assets</td>
