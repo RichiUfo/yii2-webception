7b85688d7db970de18fb18d824e79aa1d31e1af5
Theo Godard <theo.godard@gmail.com>
2015-07-16 05:08:43 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -118,25 +118,11 @@ BaseAsset::register($this);
             </thead>
             <tbody>
                 <tr>
-                    <td>Debt To Equity</td>
-                    <td class="fit-width"><span class="pull-right">47 %</span></td>
-                    <td class="text-right fit-width text-green"><i class="material-icons">trending_up</i><span class="money" value="125.12" currency=""></span></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
-                <tr>
-                    <td>Current Ratio</td>
-                    <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                    <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
-                </tr>
-                <tr>
-                    <td>Return On Assets</td>
-                    <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                    <td class="text-right fit-width text-red"><i class="material-icons">trending_down</i><span class="money" value="125.12" currency=""></span></td>
-                </tr>
-                <tr>
-                    <td>Return On Equity</td>
-                    <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                    <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
-                </tr>
+                
             </tbody>
         </table>
     </div>
