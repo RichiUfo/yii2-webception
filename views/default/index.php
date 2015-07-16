c132b5b8c5b7d001c9ced9787dd476be5132a485
Theo Godard <theo.godard@gmail.com>
2015-07-16 05:58:23 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -15,33 +15,36 @@ BaseAsset::register($this);
 <?php $this->beginBlock('page-header'); ?>
     
     <!-- Title & Dates -->
-    <div class="col-lg-12 time-range">
-        <h1>Accounting</h1>
-        <div class="right-menu">
-            <span class="icon"><i class="fa fa-calendar"></i></span> 
-            <div id="input-daterange-container">
-                <?= DateRangePicker::widget([
-                    'id' => 'input-daterange-widget',
-                    'name' => 'date_from',
-                    'size' => 'sm',
-                    'value' => date("Y-m-d", strtotime(date("Y-m-d").' -1 months')),
-                    'nameTo' => 'date_to',
-                    'valueTo' => date("Y-m-d"),
-                    'labelTo' => '<i class="fa fa-chevron-right"></i>',
-                    'clientOptions' => [
-                        'format' => 'yyyy-mm-dd',
-                        'autoclose' => true
-                    ],
-                    'clientEvents' => [
-                        'changeDate' => 'function ev(){acc_sum_refresh();}'
-                    ]
-                ]); ?>
+    <div class="row">
+        <div class="col-lg-12 time-range">
+            <h1>Accounting</h1>
+            <div class="right-menu">
+                <span class="icon"><i class="fa fa-calendar"></i></span> 
+                <div id="input-daterange-container">
+                    <?= DateRangePicker::widget([
+                        'id' => 'input-daterange-widget',
+                        'name' => 'date_from',
+                        'size' => 'sm',
+                        'value' => date("Y-m-d", strtotime(date("Y-m-d").' -1 months')),
+                        'nameTo' => 'date_to',
+                        'valueTo' => date("Y-m-d"),
+                        'labelTo' => '<i class="fa fa-chevron-right"></i>',
+                        'clientOptions' => [
+                            'format' => 'yyyy-mm-dd',
+                            'autoclose' => true
+                        ],
+                        'clientEvents' => [
+                            'changeDate' => 'function ev(){acc_sum_refresh();}'
+                        ]
+                    ]); ?>
+                </div>
             </div>
         </div>
     </div>
     
     <!-- Page Summary -->
-    <div id="page-header-summary"></div>
+    <div id="page-header-summary" class="row"></div>
+    
 <?php $this->endBlock(); ?>
 
 <!-- MAIN CONTENT -->
