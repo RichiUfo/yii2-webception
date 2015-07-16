153d663a7598fee476064d9cf3923c9581d03bb8
Theo Godard <theo.godard@gmail.com>
2015-07-16 02:30:05 +0000

    Autocommit

--- views/default/index.php
+++ views/default/index.php
@@ -46,22 +46,22 @@ BaseAsset::register($this);
             <tr>
                 <td>Assets</td>
                 <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                <td class="text-right fit-width green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
             </tr>
             <tr>
                 <td>Assets</td>
                 <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                <td class="text-right fit-width green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
             </tr>
             <tr>
                 <td>Assets</td>
                 <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                <td class="text-right fit-width green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                <td class="text-right fit-width text-red"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
             </tr>
             <tr>
                 <td>Assets</td>
                 <td class="fit-width"><span class="pull-right money" value="51,000" currency=""></span></td>
-                <td class="text-right fit-width green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="125.12" currency=""></span></td>
             </tr>
         </table>
     </div>
