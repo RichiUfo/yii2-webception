6b1c65221322772ad280ac872581e7186e2c9245
Theo Godard <theo.godard@gmail.com>
2015-07-17 07:33:45 +0000

    Autocommit

--- views/default/partial_summary_header.php
+++ views/default/partial_summary_header.php
@@ -6,7 +6,7 @@ use frontend\widgets\buttonmodal\ButtonModal;
 ?>
 
 <!-- Data Summary -->
-<div class="col-lg-4">
+<div class="col-lg-3">
     <table class="table table-condensed">
         <thead>
             <tr>
@@ -39,7 +39,7 @@ use frontend\widgets\buttonmodal\ButtonModal;
 </div>
 
 <!-- Analytics -->
-<div class="col-lg-4">
+<div class="col-lg-3">
     <table class="table table-condensed">
         <thead>
             <tr>
@@ -72,9 +72,41 @@ use frontend\widgets\buttonmodal\ButtonModal;
     
 </div>
 
+<!-- Lastest Transactions -->
+<div class="col-lg-3">
+    <table class="table table-condensed">
+        <thead>
+            <tr>
+                <th colspan="3">Latest Transactions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Debt To Equity</td>
+                <td class="fit-width"><span class="pull-right">0</span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">trending_up</i><span class="money" value="0" currency=""></span></td>
+            </tr>
+            <tr>
+                <td>Current Ratio</td>
+                <td class="fit-width"><span class="pull-right money" value="0" currency=""></span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="0" currency=""></span></td>
+            </tr>
+            <tr>
+                <td>Return On Assets</td>
+                <td class="fit-width"><span class="pull-right money" value="0" currency=""></span></td>
+                <td class="text-right fit-width text-red"><i class="material-icons">trending_down</i><span class="money" value="0" currency=""></span></td>
+            </tr>
+            <tr>
+                <td>Return On Equity</td>
+                <td class="fit-width"><span class="pull-right money" value="0" currency=""></span></td>
+                <td class="text-right fit-width text-green"><i class="material-icons">arrow_drop_up</i><span class="money" value="0" currency=""></span></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 <!-- Quick Actions -->
-<div class="col-lg-4">
+<div class="col-lg-3">
     <table class="table table-condensed quick-actions">
         <thead>
             <tr>
