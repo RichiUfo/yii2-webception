9687f8683cc296b1a936d728527ce647da2a702f
Theo Godard <theo.godard@gmail.com>
2015-07-18 04:37:31 +0000

    Autocommit

--- views/account/account.php
+++ views/account/account.php
@@ -2,59 +2,58 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use dosamigos\datepicker\DateRangePicker;
-use frontend\widgets\editable\Editable;
 
 use frontend\modules\accounting\assets\BaseAsset;
 BaseAsset::register($this);
 ?>
 
-<!-- LEFT PANEL -->
-<?php $this->render('@app/views/partials/left_panel', [
-    'back_button' => $back_button,
-    'left_menus' => $left_menus
-]); ?>
-
-<!-- MAIN CONTENT -->
-<div class="fp-acc-page">
+<!-- HEADER -->
+<?php $this->beginBlock('page-header'); ?>
     
-    <div class=" header time-range">
+    <!-- Title & Dates -->
+    <div class="row">
+        <div class="col-lg-12 time-range">
+            
+            <?= Editable::widget([
+                'identifier' => $account->id,
+                'text' => $account->alias,
+                'property' => 'alias',
+                'default' => $account->name,
+                'action' => '/accounting/rest/account/rename',    
+            ]); ?>
         
-        <?= Editable::widget([
-            'identifier' => $account->id,
-            'text' => $account->alias,
-            'property' => 'alias',
-            'default' => $account->name,
-            'action' => '/accounting/rest/account/rename',    
-        ]); ?>
-    
-        <div class="right-menu">
-            
-            <span class="icon"><i class="fa fa-calendar"></i></span> 
-            
-            <div id="input-daterange-container">
-                <?= DateRangePicker::widget([
-                    'id' => 'input-daterange-widget',
-                    'name' => 'date_from',
-                    'size' => 'sm',
-                    'value' => date("Y-m-d", strtotime(date("Y-m-d").' -1 months')),
-                    'nameTo' => 'date_to',
-                    'valueTo' => date("Y-m-d"),
-                    'labelTo' => '<i class="fa fa-chevron-right"></i>',
-                    'clientOptions' => [
-                        'format' => 'yyyy-mm-dd',
-                        'autoclose' => true
-                    ],
-                    'clientEvents' => [
-                        'changeDate' => 'function ev(){acc_acc_refresh();}'
-                    ]
-                ]); ?>
+            <div class="right-menu">
+                <span class="icon"><i class="fa fa-calendar"></i></span> 
+                <div id="input-daterange-container">
+                    <?= DateRangePicker::widget([
+                        'id' => 'input-daterange-widget',
+                        'name' => 'date_from',
+                        'size' => 'sm',
+                        'value' => date("Y-m-d", strtotime(date("Y-m-d").' -1 months')),
+                        'nameTo' => 'date_to',
+                        'valueTo' => date("Y-m-d"),
+                        'labelTo' => '<i class="fa fa-chevron-right"></i>',
+                        'clientOptions' => [
+                            'format' => 'yyyy-mm-dd',
+                            'autoclose' => true
+                        ],
+                        'clientEvents' => [
+                            'changeDate' => 'function ev(){acc_acc_refresh();}'
+                        ]
+                    ]); ?>
+                </div>
             </div>
         </div>
     </div>
     
+    <!-- Page Summary -->
+    <div id="page-header-summary" class="row"></div>
+    
+<?php $this->endBlock(); ?>
+
+<!-- MAIN CONTENT -->
+<div class="fp-acc-page">    
     <div id="accounting-account-container" class="content" accountid="<?= $account->id ?>"></div>
-    
-    
 </div>
 
 <!-- JAVASCRIPT -->
