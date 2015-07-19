85f934b906fc8f88124d91333ffea2f05eb269e4
Theo Godard <theo.godard@gmail.com>
2015-07-19 05:15:53 +0000

    Autocommit

--- views/account/create.php
+++ views/account/create.php
@@ -44,9 +44,7 @@ use frontend\widgets\stepform\StepForm;
                 </div>
                 <div class="col-lg-5">
                     <h4 class="info-text">New Account</h4>
-                    <?= $stepform->form->field($model, 'number', [
-                        'ng-model' => 'account_number'
-                    ]) ?>
+                    <?= $stepform->form->field($model, 'number')->input('text', ['ng-model' => 'account_number']) ?>
                     <?= $stepform->form->field($model, 'name') ?>
                     <?= $stepform->form->field($model, 'currency') ?> 
                 </div>
