421c16b784249d01d509dff7b0bc5ba00a1f7a19
Theo Godard <theo.godard@gmail.com>
2015-07-20 07:25:58 +0000

    Autocommit

--- views/transaction/create.php
+++ views/transaction/create.php
@@ -117,7 +117,7 @@ BaseAsset::register($this);
 				<div ng-if="account_credit.currency.code == account_debit.currency.code">
 					<div class="form-group field-transaction-value required">
 						<label class="control-label" for="transaction-value">Value</label>
-						<div class="input-group m-b">
+						<div class="input-group m-b" ng-show="account_debit">
 	      					<input type="text" id="transaction-value" class="form-control" name="Transaction[value]" placeholder="">
 	      					<div class="input-group-addon right" ng-show="account_debit">
 	      						{{account_debit.currency.code}}&nbsp;
