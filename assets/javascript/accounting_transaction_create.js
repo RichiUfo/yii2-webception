1861f5a2622349c26896fa68f5d4a51d3f6eec9f
Theo Godard <theo.godard@gmail.com>
2015-07-20 06:38:03 +0000

    Autocommit

--- assets/javascript/accounting_transaction_create.js
+++ assets/javascript/accounting_transaction_create.js
@@ -8,12 +8,13 @@ app.controller("FormController", ['$scope', '$http', function($scope, $http) {
 	// Variables
 	$scope.account_debit = null;
 	$scope.account_credit = null;
+	$scope.account_debit_id = null;
+	$scope.account_credit_id = null;
 	$scope.transaction_title = '';
 	$scope.error = {invalid:true, msg:''};
 	
 	// Get the account information when changed
 	$scope.$watch('account_debit_id', function(value) {
-		console.log('account_debit_id has changed', $scope.account_debit_id);
 		if($scope.account_debit_id !== null){
 			$http.get('/accounting/account/get-account-summary', {
 				params: { accountid: $scope.account_debit_id }
@@ -30,7 +31,6 @@ app.controller("FormController", ['$scope', '$http', function($scope, $http) {
 		}
 	}); 
 	$scope.$watch('account_credit_id', function(value) {
-		console.log('account_credit_id has changed', $scope.account_credit_id);
 		if($scope.account_credit_id !== null){
 			$http.get('/accounting/account/get-account-summary', {
 				params: { accountid: $scope.account_credit_id }
