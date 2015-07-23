e7ad4e7542a757b483a093dbb04e7d078c46ca78
Theo Godard <theo.godard@gmail.com>
2015-07-23 02:00:22 +0000

    Autocommit

--- widgets/overview/OverviewEntity.php
+++ widgets/overview/OverviewEntity.php
@@ -10,6 +10,12 @@ use frontend\widgets\box\Box;
 class OverviewEntity extends \frontend\widgets\box\Box {
 
     public function init(){
+        parent::init();
+		
+		// Starts output buffering
+        ob_start();
+        ob_implicit_flush(false);
+        
         $this->title = 'Accounting';
         $this->route = '/accounting';
 	}
