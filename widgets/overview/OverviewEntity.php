9b6780145afe256e57d1ff491a18f6393cd6894b
Theo Godard <theo.godard@gmail.com>
2015-07-23 00:02:04 +0000

    Autocommit

--- widgets/overview/OverviewEntity.php
+++ widgets/overview/OverviewEntity.php
@@ -5,42 +5,20 @@ namespace frontend\modules\accounting\widgets\overview;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
-class OverviewEntity extends \yii\base\Widget{
+use frontend\widgets\box\Box;
+
+class OverviewEntity extends Box {
 
     // Initialize the widget
     public function init(){
         parent::init();
-        
-        // Assets Registration
-		//OverviewAsset::register($this->getView());
-		
-		// Starts output buffering
-        ob_start();
-        ob_implicit_flush(false);
-        
-        // Render the container
-		echo Html::beginTag('div', ['class' => 'col-xs-12 col-sm-4 col-md-4 col-lg-4 account-overview']);
-		echo Html::beginTag('div', ['class' => 'box']);
-		
-		echo Html::beginTag('span', ['class' => 'title']);
-		echo Html::tag('a', 'Accounting', ['href' => Url::toRoute('/accounting')]);
-		echo Html::endTag('span');
-		
-		echo Html::beginTag('div', ['class' => 'row overview-row']);
-		echo Html::beginTag('div', ['class' => 'col-lg-12']);
-        
 	}
 	
 	// Rendering
     public function run(){
-		
 		echo 'Widget in development ...';
 		
-		echo Html::endTag('div');
-		echo Html::endTag('div');
-		echo Html::endTag('div');
-		echo Html::endTag('div');
-		
+		parent::run();
 		return ob_get_clean();
     }
     
