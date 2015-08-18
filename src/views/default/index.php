86a1248052d07d57d1b49ae328e75428df58c568
Theo Godard <theo.godard@gmail.com>
2015-08-18 15:38:37 +0000

    modify the index page

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -7,5 +7,5 @@ Yii2WebceptionAsset::register($this);
 ?>
 
 <div class="container">
-    <h1>Yii2 Webception<small>Code Testing Extension</small></h1>
+    <h1>Yii2 Webception - Code Testing Extension<</h1>
 </div>
