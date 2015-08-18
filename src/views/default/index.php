0529dee1ebfe156a43396a1b5bbb7dda6cace132
Theo Godard <theo.godard@gmail.com>
2015-08-18 10:41:22 +0000

    modify the index page

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -5,3 +5,7 @@ use yii\helpers\Url;
 use godardth\yii2webception\assets\Yii2WebceptionAsset;
 Yii2WebceptionAsset::register($this);
 ?>
+
+<div>
+    <h1>Yii2 Webception<small>Code Testing Extension</small></h1>
+</div>
