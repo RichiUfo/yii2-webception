224c4f5921b97e2fbbb024f2c458070ddb2caa67
Theo Godard <theo.godard@gmail.com>
2015-08-19 03:56:36 +0000

    Implementing the index route

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -9,6 +9,7 @@ Yii2WebceptionAsset::register($this);
 <div class="container">
     <h1>Webception</h1>
     <h2>Test Sites</h2>
-    Ready : <?= $ready?'Ready':'Not Ready'; ?><br>
-    Test count : <?= $test_count ?>
+    <?= $ready?'Ready':'Not Ready'; ?><br>
+    Test count : <?= $test_count ?><br><br>
+    codeception object : <br> <?php var_dump($codeception); ?>
 </div>
