d8dcdcde086fc9603714d3baf3d0baa03ea00e4b
Theo Godard <theo.godard@gmail.com>
2015-08-20 14:44:10 +0000

    Development commit

--- src/models/Site.php
+++ src/models/Site.php
@@ -95,7 +95,7 @@ class Site extends \yii\db\ActiveRecord
                     // Declare a new test and add it to the list.
                     $test = new Test();
                     //$test->init($type, $file);
-                    $test->title = $file;
+                    $test->title = $file->getFilename();
                     $this->addTest($test);
                     unset($test);
                 }
