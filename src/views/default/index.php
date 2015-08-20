7c7fe6728fd8afa1c4007ad6f3d123794395c82f
Theo Godard <theo.godard@gmail.com>
2015-08-20 15:04:27 +0000

    Development commit

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -8,20 +8,14 @@ Yii2WebceptionAsset::register($this);
 
 <div class="container">
     <h1>Webception</h1>
-    <h2>Test Sites</h2>
-    <!--?= $ready?'Ready':'Not Ready'; ?><br>
-    Test count : ?= $test_count ?><br><br>
-    codeception object : <br> ?php var_dump($codeception); ?-->
-    
     
     <!-- Check the codeception initialization -->
-    
     
     <!-- List the available sites and tests for each of them -->
     <br><br> Available Sites <br>
     <ul>
     <?php foreach($sites as $site) : ?>
-        <li><?= $site->name ?> : <?= $site->config ?></li>
+        <li><?= $site->name ?></li>
         <ul>
             <?php foreach($site->tests as $test) : ?>
                 <li><?= $test->title ?></li>
