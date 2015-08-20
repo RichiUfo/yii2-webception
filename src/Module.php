7ddb1cd96aeb41c60fc4dd66b83a91b8ab7bc9ce
Theo Godard <theo.godard@gmail.com>
2015-08-20 23:53:51 +0000

    Development commit

--- src/Module.php
+++ src/Module.php
@@ -35,5 +35,12 @@ class Module extends \yii\base\Module
 		$this->db->open();
 		
     }
+    
+    public function init()
+	{
+	    parent::init();
+	    // initialize the module with the configuration loaded from config.php
+	    \Yii::configure($this, require(__DIR__ . '/config.php'));
+	}
 		
 }
