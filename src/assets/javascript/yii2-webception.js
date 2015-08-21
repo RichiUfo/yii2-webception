20984d6cc875108c1107f752c1cbee85e1ed31fc
Theo Godard <theo.godard@gmail.com>
2015-08-21 04:12:00 +0000

    Development commit

--- src/assets/javascript/yii2-webception.js
+++ src/assets/javascript/yii2-webception.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         console.log('Trying to run a test with hash ' + $(this).attr('hash'));
         
         $.ajax({
-            url: "test/run-test", 
+            url: "testing/test/run-test", 
             success: function(result){
                 console.log('Test executed. Output ' + result);
             }
