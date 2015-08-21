ae896f893ef769e0bdc5e297b31fa8cfd71e6788
Theo Godard <theo.godard@gmail.com>
2015-08-21 07:50:32 +0000

    Development commit

--- src/assets/javascript/yii2-webception.js
+++ src/assets/javascript/yii2-webception.js
@@ -11,7 +11,7 @@ var runTest = function(hash) {
             console.log(result)
             $('#'+hash+' .status').html(result.state)
             if(result.state == 'Failed') {
-                console.log('Failed')
+                console.log($('#'+hash+' .status').closest('.test-log td pre'))
                 $('#'+hash+' .status').closest('.test-log td pre').html(result.log)
             }
         }
