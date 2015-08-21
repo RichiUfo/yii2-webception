eb6165c77f7178ee132913cc433f77e2098a20e2
Theo Godard <theo.godard@gmail.com>
2015-08-21 07:02:35 +0000

    Development commit

--- src/assets/javascript/yii2-webception.js
+++ src/assets/javascript/yii2-webception.js
@@ -1,19 +1,20 @@
+var runTest = function(hash) {
+    $.ajax({
+        type: "GET",
+        url: "testing/test/run-test", 
+        data: { hash: hash },
+        dataType: "json",
+        success: function(result){
+            $('#'+hash+' .status').html(result.state)
+        }
+    })
+}
+
 $(document).ready(function(){
     
     $('.run-test').click(function(){
-        
-        var hash = $(this).attr('hash');
-        
-        $.ajax({
-            ty: "GET",
-            url: "testing/test/run-test", 
-            data: { hash: hash },
-            dataType: "json",
-            success: function(result){
-                $('#'+hash+' .status').html(result.state);
-            }
-        });
-        
+        var hash = $(this).attr('hash')
+        runTest(hash)
     })
     
-});
+})
