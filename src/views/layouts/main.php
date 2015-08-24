5ea89d7fe22b4d63c79bb0b8eac56c80db69d96c
Theo Godard <theo.godard@gmail.com>
2015-08-24 12:23:24 +0000

    Development commit

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -6,7 +6,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
+//use common\widgets\Alert;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
@@ -57,7 +57,7 @@ AppAsset::register($this);
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
-        <?= Alert::widget() ?>
+        <!--= Alert::widget() ?-->
         <?= $content ?>
     </div>
 </div>
