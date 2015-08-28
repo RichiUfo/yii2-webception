c30dfc1d7d49b8d353125a2170949423688c6174
Theo Godard <theo.godard@gmail.com>
2015-08-28 06:28:04 +0000

    Development commit

--- src/models/Coverage.php
+++ src/models/Coverage.php
@@ -6,50 +6,6 @@ use Yii;
 use yii\helpers\Url;
 
 /**
- * Defining the types stored in coverage.xml
- */
-class CovBaseElement {
-    public $children = [];
-}
-class CovCoverage extends BaseElement {
-    public $generated;
-}
-class CovProject extends BaseElement {
-    public $timestamp;
-}
-class CovPackage extends BaseElement {
-    public $name;
-}
-class CovFile extends BaseElement {
-    public $name;
-}
-class CovClass extends BaseElement {
-    public $name;
-    public $namespace;
-}
-class CovMetrics  extends BaseElement {
-    public $methods;
-    public $coveredmethods;
-    public $conditionals;
-    public $coveredconditionals;
-    public $statements;
-    public $coveredstatements;
-    public $elements;
-    public $coveredelements;
-    public $loc;
-    public $ncloc;
-    public $classes;
-    public $files;
-}
-class CovLine extends BaseElement {
-    public $num;
-    public $type;
-    public $name;
-    public $crap;
-    public $count;
-}
-
-/**
  * This is the model class for Codeception.
  */
 class Coverage extends \yii\base\Model
