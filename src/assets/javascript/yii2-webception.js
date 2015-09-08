8f41cdfd3b7f92f6d65b62c69036fe0971541a96
Theo Godard <theo.godard@gmail.com>
2015-09-08 05:08:58 +0000

    Development commit

--- src/assets/javascript/yii2-webception.js
+++ src/assets/javascript/yii2-webception.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
         var site = $(this).attr('site')
         var type = $(this).attr('type')
         testQueue = $('#'+site+' .'+type+' .run-test')
-        console.log(testQueue[0])
+        console.log($(testQueue[0]).attr('hash'))
         //runTest()
     })
     
